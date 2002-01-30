f55576949c0fdc7c99ccb8e77c080f7f59baecdf
Daniel Naber <daniel.naber@t-online.de>
2002-01-30 14:29:31 +0000

    script to extract addAuthor() information from c++ files and print them out as a DocBook list
    svn path=/trunk/kdesdk/scripts/; revision=134676

---
+++ authors2xml.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+# Extract author information from C++ files
+# and print it out in DocBook format as a list
+# Daniel Naber <daniel.naber@t-online.de>
+# $Id: $
+
+my $file = $ARGV[0];
+if( ! $file ) {
+	print "Usage: $0 <file.cpp>\n";
+	exit;
+}
+
+open(IN, $file) || die "Cannot open '$file': $!\n";
+undef $/;
+my $str = (<IN>);
+close(IN);
+
+print "<itemizedlist>\n";
+while( $str =~ m/addAuthor\s*\(\s*"(.*?)",\s*.*?,\s*"(.*?)"/gs ) {
+	my ($name, $email) = ($1, $2);
+	print "<listitem><para>$name <email>$email</email></para></listitem>\n";
+	#print "$name, $email\n";
+}
+print "</itemizedlist>\n";
+
+print STDERR "Warning: maybe you need to fix umlauts manually...\n";
+exit;
