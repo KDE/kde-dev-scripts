56d3d535188a2bb664384bc29c06dab66e70d146
Stephan Kulow <coolo@kde.org>
2003-02-11 14:31:07 +0000

    commiting Willy De's patch to cleanup the logging and call configure with relative path for srcdir == builddir
    svn path=/trunk/kdesdk/scripts/; revision=206287

---
+++ build-progress.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# This method gives some kind of status message in the title bar of Konsole,
+# xterm, etc.. Thanks have to go to Malte Starostik
+# <malte@kde.org> for the code :-)
+set_title() {
+if ([ "$TERM" = "xterm" ] || [ "$TERM" = "xterm-color" ] || [ "$TERM" = "screen" ]) && tty -s; then
+  echo -ne "\033]0;$1\007"
+fi
+}
+. ./kde-buildrc
+set_title "Progres of kde-build script..."
+
+cd $KDELOGDIR
+while true; do 
+    dir=`ls -t | head -1 | xargs grep "Entering directory" | tail -1 | awk "{print \\$4}" | sed "s'$KDESRCDIR/''g"`
+    set_title "Building $dir"
+    clear
+    grep -i "time needed" *build* | \
+	sed "s/\:T/ T/g" | \
+	awk "{ print \$2, \$3, \$4, \$5, \$6, \$7, \$8, \$9}"
+    sleep 5
+done;
