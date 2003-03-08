78feb5199c760a946fb42bc4ae8d74ea12f10de9
Waldo Bastian <bastian@kde.org>
2003-03-08 01:43:08 +0000

    CVS_SILENT Updated address of FSF
    svn path=/trunk/kdesdk/scripts/; revision=212247

--- kde-emacs/klaralv.el
+++ kde-emacs/klaralv.el
@@ -14,7 +14,7 @@
 ;;
 ;; You should have received a copy of the GNU General Public License along
 ;; with GNU Emacs.  If you did not, write to the Free Software Foundation,
-;; Inc., 675 Mass Ave., Cambridge, MA 02139, USA.
+;; Inc., 675 Mass Ave., Cambridge, MA 02111-1307, USA.
 
 
 ;; ------------------------------ INSTALLATION ------------------------------
