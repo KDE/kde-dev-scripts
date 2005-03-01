0ba7bbd1cfcfcbba0d599fde46a0d4aebcf409ed
Lukáš Tinkl <lukas@kde.org>
2005-03-01 08:50:42 +0000

    Killer feature :)
    Bind "Enter" key to the CC mode function c-context-line-break which
    does the right thing (tm) depending on the context:
    
    RET runs the command c-context-line-break
    which is an interactive compiled Lisp function in `cc-cmds'.
    (c-context-line-break)
    
    Do a line break suitable to the context.
    When point is outside a comment or macro, insert a newline and
    indent according to the syntactic context, unless `c-syntactic-indentation'
    is nil, in which case the new line is indented as the previous
    non-empty line instead.
    When point is inside the content of a preprocessor directive, a line
    continuation backslash is inserted before the line break and aligned
    appropriately. The end of the cpp directive doesn't count as inside
    it.When point is inside a comment, continue it with the appropriate
    comment prefix (see the `c-comment-prefix-regexp' and
    `c-block-comment-prefix' variables for details).
    The end of a C++-style line comment doesn't count as inside it.
    
    svn path=/branches/KDE_3_4_BRANCH/kdesdk/scripts/; revision=394032

--- kde-emacs/kde-emacs-core.el
+++ kde-emacs/kde-emacs-core.el
@@ -142,7 +142,7 @@ With arg, do it arg times."
 (defun kde-c++-mode-hook ()
   (font-lock-mode)
   (c-set-style kde-c++-style)
-  (define-key c++-mode-map "\C-m" 'newline-and-indent)
+  (define-key c++-mode-map "\C-m" 'c-context-line-break)
   (when (or
 	 (eq kde-tab-behavior 'default)
 	 (eq kde-tab-behavior 'abbrev-indent))
