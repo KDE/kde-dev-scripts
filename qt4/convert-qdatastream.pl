0d274a66db9fcce100e32ff66c81504143c2530a
Laurent Montel <montel@kde.org>
2005-07-19 09:51:07 +0000

    Add script to convert to new QDataStream  API
    svn path=/trunk/KDE/kdesdk/scripts/; revision=436208

---
+++ qt4/convert-qdatastream.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl 
+
+# Montel Laurent <montel@kde.org>
+# This script allows to convert QDataStream to new API
+
+use lib qw( . );
+use functionUtilkde; 
+
+foreach my $file (@ARGV) {
+    functionUtilkde::substInFile {
+	if (my ($blank, $prefix, $contenu) = m!^(\s*)(.*QDataStream.*)\((.*)\s*\);$!) {
+	    if ( my ($firstelement, $secondelement) = m!.*\(\s*(.*),\s*(.*)\);\s*$!) {
+		if ( substr($firstelement, 0, 1) ne "&") 
+		{
+		    $_ = $blank . $prefix . "( &" . $firstelement . "," . $secondelement . ");\n" ;
+		}
+	    }
+	}
+	
+    } $file;
+
+}
+functionUtilkde::diffFile( "@ARGV" );
