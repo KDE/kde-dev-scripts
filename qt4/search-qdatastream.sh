039a67cbc5d352e1392634810eb17a989485300e
Laurent Montel <montel@kde.org>
2005-07-19 13:13:40 +0000

    Don't search into generated files
    svn path=/branches/work/kde4/kdesdk/scripts/; revision=436291

--- qt4/search-qdatastream.sh
+++ qt4/search-qdatastream.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-grep -rl "QDataStream " * |grep -v "\.svn" |grep -v "\.libs" | grep -v "\.o" | grep -v Makefile | grep -v Makefile.in  | grep -v "\.moc" | grep -v "\.lo" | grep -v "\.la" |grep -v "\.Ulo"
+grep -rl "QDataStream " * |grep -v "\.svn" |grep -v "\.libs" | grep -v "\.o" | grep -v Makefile | grep -v Makefile.in  | grep -v "\.moc" | grep -v "\.lo" | grep -v "\.la" |grep -v "\.Ulo" | grep -v "_skel.cpp" | grep -v "_stub.cpp"
