2be33451bc05fa482db1dd608e42aff9ee99e22d
Laurent Montel <montel@kde.org>
2005-08-29 12:52:15 +0000

    Fix my script:
    svn path=/trunk/KDE/kdesdk/scripts/; revision=454603

--- qt4/adapte-to-kde4-api.pl
+++ qt4/adapte-to-kde4-api.pl
@@ -46,7 +46,7 @@ foreach my $file (@ARGV) {
 	s!b6Pressed!resumePressed!;
 	s!b7Pressed!resumeAllPressed!;
 	#KMainWindow
-        s!(?<!KMainWindow::memberList\(\))KMainWindow::memberList!KMainWindow::memberList()!;	
+        s/(?<!KMainWindow::memberList\(\))KMainWindow::memberList/KMainWindow::memberList()/;	
 	s!KMainWindow::getMemberList!KMainWindow::memberList!;
 	} $file;
 }
