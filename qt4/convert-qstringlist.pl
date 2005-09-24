ebcca03efcf3597f65317658a6de5c82b8c3da45
Laurent Montel <montel@kde.org>
2005-09-24 08:49:05 +0000

    Support complete convert of QStringList::split to QString::split => support three element and QRegExp function
    svn path=/trunk/KDE/kdesdk/scripts/; revision=463470

--- qt4/convert-qstringlist.pl
+++ qt4/convert-qstringlist.pl
@@ -10,8 +10,45 @@ use functionUtilkde;
 foreach my $file (@ARGV) {
     functionUtilkde::substInFile {
     if (my ($blank, $prefix, $contenu) = m!^(\s*.*)(QStringList::split.*)\((.*)\s*\);$!) {
-			#warn "blank : $blank, prefix : $prefix, contenu : $contenu \n";
-        	if ( my ($firstelement, $secondelement) = m!.*\(\s*(.*),\s*(.*)\);\s*$!) {
+			warn "blank : $blank, prefix : $prefix, contenu : $contenu \n";
+			if ( my ($firstelement, $secondelement, $thirdelement) = m!.*?\(\s*(.*),\s*(.*),\s*(.*)\);\s*$!) {
+					warn "three element : first : $firstelement : second $secondelement : third $thirdelement \n";
+					my $argument = $prefix;
+                    # Remove space before argument
+                    $secondelement =~ s/ //g;	
+					$thirdelement =~ s/ //g;
+					if ( $blank =~ /insertStringList/ ) {
+							$thirdelement =~ s/\)//g;
+                        if ( $thirdelement =~ /true/ ) {
+                            #QString::KeepEmptyParts
+                            $_ = $blank . $secondelement . ".split( " . $firstelement . ", QString::KeepEmptyParts" . "));\n" ;
+                        }
+                        elsif ( $thirdelement =~ /false/ ) {
+                            #QString::SkipEmptyParts
+                            $_ = $blank . $secondelement . ".split( " . $firstelement . ", QString::SkipEmptyParts" . "));\n" ;
+                        }
+                        # different element
+                        else {
+                            $_ = $blank . $secondelement . ".split( " . $firstelement . ", $thirdelement" . "));\n" ;
+                    }	
+                    }
+                    else {
+						if ( $thirdelement =~ /true/ ) {
+							#QString::KeepEmptyParts
+							$_ = $blank . $secondelement . ".split( " . $firstelement . ", QString::KeepEmptyParts" . ");\n" ;	
+						}
+						elsif ( $thirdelement =~ /false/ ) {
+							#QString::SkipEmptyParts
+							$_ = $blank . $secondelement . ".split( " . $firstelement . ", QString::SkipEmptyParts" . ");\n" ;
+						}
+						# different element
+						else {
+							$_ = $blank . $secondelement . ".split( " . $firstelement . ", $thirdelement" . ");\n" ;
+					}
+					}
+
+			}
+        	elsif ( my ($firstelement, $secondelement) = m!.*?\(\s*(.*),\s*(.*)\);\s*$!) {
 					my $argument = $prefix;
 					# Remove space before argument
 					$secondelement =~ s/ //g;
