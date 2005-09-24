25fba4af759d2ee1249cf6bc343667dbe8ae5485
Laurent Montel <montel@kde.org>
2005-09-24 11:51:51 +0000

    Don't remplace into desktop file
    svn path=/trunk/KDE/kdesdk/scripts/; revision=463528

--- qt4/search-qstring.sh
+++ qt4/search-qstring.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-egrep -rl 'findRev|local8Bit|utf8' * |grep -v "\.svn" |grep -v "\.libs" | grep -v "\.o" | grep -v Makefile | grep -v Makefile.in  | grep -v "\.moc" | grep -v "\.lo" | grep -v "\.la" |grep -v "\.Ulo" |grep -v "\.kidl"
+egrep -rl 'findRev|local8Bit|utf8' * |grep -v "\.svn" |grep -v "\.libs" | grep -v "\.o" | grep -v Makefile | grep -v Makefile.in  | grep -v "\.moc" | grep -v "\.lo" | grep -v "\.la" |grep -v "\.Ulo" |grep -v "\.kidl" | grep -v "\.desktop"
