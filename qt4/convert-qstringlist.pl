28e12b3cafab2aec5dfec5a0648a20dab16ee4a7
Laurent Montel <montel@kde.org>
2005-09-27 12:23:59 +0000

    By default when we use QRegExp we must add second element
    svn path=/trunk/KDE/kdesdk/scripts/; revision=464459

--- qt4/convert-qstringlist.pl
+++ qt4/convert-qstringlist.pl
@@ -3,23 +3,30 @@
 # laurent Montel <montel@kde.org>
 # this function changes QStringList::split (QT3_SUPPORT) to QString::split (QT4)
 
-
 use lib qw( . );
 use functionUtilkde; 
+
+sub addQStringElement
+{
+	my $result = $_[0];
+	if ( $result =~ /^\"/ ) {
+		$result = "QString( " . $result . ")";
+	}
+	return $result;
+};
 
 foreach my $file (@ARGV) {
     functionUtilkde::substInFile {
     if (my ($blank, $prefix, $contenu) = m!^(\s*.*)(QStringList::split.*)\((.*)\s*\);$!) {
-			warn "blank : $blank, prefix : $prefix, contenu : $contenu \n";
+			#warn "blank : $blank, prefix : $prefix, contenu : $contenu \n";
 			if ( my ($firstelement, $secondelement, $thirdelement) = m!.*?\(\s*(.*),\s*(.*),\s*(.*)\);\s*$!) {
-					warn "three element : first : $firstelement : second $secondelement : third $thirdelement \n";
+					#warn "three element : first : $firstelement : second $secondelement : third $thirdelement \n";
 					my $argument = $prefix;
                     # Remove space before argument
                     $secondelement =~ s/ //g;	
 					$thirdelement =~ s/ //g;
-					if ( $secondelement =~ /^\"/ ) {
-							$secondelement = "QString( " . $secondelement . ")";
-					}
+					
+					$secondelement = addQStringElement( $secondelement );
 					if ( $blank =~ /insertStringList/ ) {
 							$thirdelement =~ s/\)//g;
                         if ( $thirdelement =~ /true/ ) {
@@ -55,15 +62,25 @@ foreach my $file (@ARGV) {
 					my $argument = $prefix;
 					# Remove space before argument
 					$secondelement =~ s/ //g;
-					if ( $secondelement =~ /^\"/ ) {
-							$secondelement = "QString( " . $secondelement . ")";
-                    }
-					if ( $blank =~ /insertStringList/ ) {
-							$secondelement =~ s/\)//g;
-							$_ = $blank . $secondelement . ".split( " . $firstelement . "));\n" ;
+					$secondelement = addQStringElement( $secondelement ); 
+					if ($firstelement ~= /QRegExp/ ) {
+                        if ( $blank =~ /insertStringList/ ) {
+                                $secondelement =~ s/\)//g;
+                                $_ = $blank . $secondelement . ".split( " . $fir
+stelement . " QString::SkipEmptyParts));\n" ;
+                        }
+                        else {
+                            $_ = $blank . $secondelement . ".split( " . $firstel
+ement . "QString::SkipEmptyParts);\n" ;
 					}
 					else {
-            			$_ = $blank . $secondelement . ".split( " . $firstelement . ");\n" ;
+						if ( $blank =~ /insertStringList/ ) {
+								$secondelement =~ s/\)//g;
+								$_ = $blank . $secondelement . ".split( " . $firstelement . "));\n" ;
+						}
+						else {
+            				$_ = $blank . $secondelement . ".split( " . $firstelement . ");\n" ;
+					}
 				}
         	}
     	}
