f01d35df167d4f314b69cb790df16fa15bda1c50
Laurent Montel <montel@kde.org>
2005-10-04 12:18:24 +0000

    It's not possible... I must commit just cervisia changes... => reverse all not necessary changes
    svn path=/trunk/KDE/kdesdk/scripts/; revision=467156

--- qt4/adapte-to-kde4-api.pl
+++ qt4/adapte-to-kde4-api.pl
@@ -65,61 +65,57 @@ while ($file = <$F>) {
 		$changes =~ s!Q3HBox!KHBox!;
 		$_ = $changes . $suite . $end . "\n";
 	    }
-	    s!#include <kacceleratormanager.h>!#include <kacceleratormanager.h>!;
-	    s!KStringHandler::matchFileName!KStringHandler::matchFileName!;
-	    if ( $_ =~ /KRandom::random/ ) {
-		s!KRandom::random!KRandom::random!;
+	    s!#include <kaccelmanager.h>!#include <kacceleratormanager.h>!;
+	    s!KStringHandler::matchFilename!KStringHandler::matchFileName!;
+	    if ( $_ =~ /KApplication::random/ ) {
+		s!KApplication::random!KRandom::random!;
 		$necessaryToAddIncludeRandom = 1;
 	    }
-	    s!KFind::WholeWordsOnly!KFind::WholeWordsOnly!;
-	    s!KFind::FromCursor!KFind::FromCursor!;
-	    s!KFind::SelectedText!KFind::SelectedText!;
-	    s!KFind::CaseSensitive!KFind::CaseSensitive!;
-	    s!KFind::FindBackwards!KFind::FindBackwards!;
-	    s!KFind::RegularExpression!KFind::RegularExpression!;
-	    s!KFind::FindIncremental!KFind::FindIncremental!;
-	    s!KFind::MinimumUserOption!KFind::MinimumUserOption!;
-	    s!kdatetable.h!kdatetable.h!;
-		s!KMenu!KMenu!g;
+	    s!KFindDialog::WholeWordsOnly!KFind::WholeWordsOnly!;
+	    s!KFindDialog::FromCursor!KFind::FromCursor!;
+	    s!KFindDialog::SelectedText!KFind::SelectedText!;
+	    s!KFindDialog::CaseSensitive!KFind::CaseSensitive!;
+	    s!KFindDialog::FindBackwards!KFind::FindBackwards!;
+	    s!KFindDialog::RegularExpression!KFind::RegularExpression!;
+	    s!KFindDialog::FindIncremental!KFind::FindIncremental!;
+	    s!KFindDialog::MinimumUserOption!KFind::MinimumUserOption!;
+	    s!kdatetbl.h!kdatetable.h!;
+		s!KPopupMenu!KMenu!g;
 	    #TODO test it, perhaps remove all before isRestored (for example if( kapp-> isRestored())
-	    s!kapp->isSessionRestored!kapp->isSessionRestored!;
-	    s!#include <kuniqueapplication.h>!#include <kuniqueapplication.h>!;
-	    s!#include <kapplication.h>!#include <kapplication.h>!;
-	    s!#include <kstandarddirs.h>!#include <kstandarddirs.h>!;
+	    s!kapp->isRestored!kapp->isSessionRestored!;
+	    s!#include <kuniqueapp.h>!#include <kuniqueapplication.h>!;
+	    s!#include <kapp.h>!#include <kapplication.h>!;
+	    s!#include <kstddirs.h>!#include <kstandarddirs.h>!;
 	    s!#include <kcodecs.h>!#include <kcodecs.h>!;
 		#kde3support
-		s!#include <k3dockwidget.h>!#include <k3dockwidget.h>!;
-		s!#include <k3mdichildarea.h>!#include <k3mdichildarea.h>!;
-		s!#include <k3mdichildfrmcaption.h>!#include <k3mdichildfrmcaption.h>!;
-		s!#include <k3mdichildfrm.h>!#include <k3mdichildfrm.h>!;
-		s!#include <k3mdichildview.h>!#include <k3mdichildview.h>!;
-		s!#include <k3mdidefines.h>!#include <k3mdidefines.h>!;
-		s!#include <k3mdidockcontainer.h>!#include <k3mdidockcontainer.h>!;
-		s!#include <k3mdidocumentviewtabwidget.h>!#include <k3mdidocumentviewtabwidget.h>!;
-		s!#include <k3mdifocuslist.h>!#include <k3mdifocuslist.h>!;
-		s!#include <k3mdiguiclient.h>!#include <k3mdiguiclient.h>!;
-		s!#include <k3mdiiterator.h>!#include <k3mdiiterator.h>!;
-		s!#include <k3mdilistiterator.h>!#include <k3mdilistiterator.h>!;		
-		s!#include <k3mdimainfrm.h>!#include <k3mdimainfrm.h>!;
-		s!#include <k3mdinulliterator.h>!#include <k3mdinulliterator.h>!;
-		s!#include <k3mditaskbar.h>!#include <k3mditaskbar.h>!;
-		s!#include <k3mditoolviewaccessor.h>!#include <k3mditoolviewaccessor.h>!;
+		s!#include <kdockwidget.h>!#include <k3dockwidget.h>!;
+		s!#include <kmdichildarea.h>!#include <k3mdichildarea.h>!;
+		s!#include <kmdichildfrmcaption.h>!#include <k3mdichildfrmcaption.h>!;
+		s!#include <kmdichildfrm.h>!#include <k3mdichildfrm.h>!;
+		s!#include <kmdichildview.h>!#include <k3mdichildview.h>!;
+		s!#include <kmdidefines.h>!#include <k3mdidefines.h>!;
+		s!#include <kmdidockcontainer.h>!#include <k3mdidockcontainer.h>!;
+		s!#include <kmdidocumentviewtabwidget.h>!#include <k3mdidocumentviewtabwidget.h>!;
+		s!#include <kmdifocuslist.h>!#include <k3mdifocuslist.h>!;
+		s!#include <kmdiguiclient.h>!#include <k3mdiguiclient.h>!;
+		s!#include <kmdiiterator.h>!#include <k3mdiiterator.h>!;
+		s!#include <kmdilistiterator.h>!#include <k3mdilistiterator.h>!;		
+		s!#include <kmdimainfrm.h>!#include <k3mdimainfrm.h>!;
+		s!#include <kmdinulliterator.h>!#include <k3mdinulliterator.h>!;
+		s!#include <kmditaskbar.h>!#include <k3mditaskbar.h>!;
+		s!#include <kmditoolviewaccessor.h>!#include <k3mditoolviewaccessor.h>!;
         if( /K3ColorDrag/ ) {
 			s!K3ColorDrag!K3ColorDrag!g;
 			$warning = $warning . "Necessary to add \$\(LIB_KDE3SUPPORT\) into Makefile.am when $file is \n";
 		}
 	
-		s!#include <k3colordrag.h>!#include <k3colordrag.h>!;
-		s!K3ColorDrag!K3ColorDrag!g;
+		s!#include <kcolordrag.h>!#include <k3colordrag.h>!;
+		s!KColorDrag!K3ColorDrag!g;
 
 	    s!KStartupInfo::appStarted!KStartupInfo::appStarted!;
 	    s!KInputDialog::getText!KInputDialog::getText!;
 	    s!#include <kde_file.h>!#include <kde_file.h>!;
-		s!#include <kmenu.h>!#include <kmenu.h>!;
-#include <QX11Info>
-#include <ktoolinvocation.h>
-#include <krandom.h>
-#include <kauthorized.h>
+		s!#include <kpopupmenu.h>!#include <kmenu.h>!;
 	    s!cancelPressed!cancelPressed!;
 	    s!suggestNewNamePressed!suggestNewNamePressed!;
 	    s!renamePressed!renamePressed!;
@@ -129,11 +125,11 @@ while ($file = <$F>) {
 	    s!overwriteAllPressed!overwriteAllPressed!;
 	    s!resumePressed!resumePressed!;
 	    s!resumeAllPressed!resumeAllPressed!;
-		s!KLocale::setMainCatalog!KLocale::setMainCatalog!;
+		s!KLocale::setMainCatalogue!KLocale::setMainCatalog!;
 		s!KGlobal::locale\(\)->insertCatalogue!KGlobal::locale\(\)->insertCatalog!;
 		s!KGlobal::locale\(\)->setMainCatalogue!KGlobal::locale\(\)->setMainCatalog!;
 		
-		s!KLocale::removeCatalog!KLocale::removeCatalog!;
+		s!KLocale::removeCatalogue!KLocale::removeCatalog!;
 		s!KGlobal::locale\(\)->removeCatalog!KGlobal::locale\(\)->removeCatalog!;
 		s!KGlobal::locale\(\)->removeCatalog!KGlobal::locale\(\)->removeCatalog!;
 
@@ -141,63 +137,63 @@ while ($file = <$F>) {
         s!KGlobal::locale\(\)->setActiveCatalog!KGlobal::locale\(\)->setActiveCatalog!;
         s!KGlobal::locale\(\)->setActiveCatalog!KGlobal::locale\(\)->setActiveCatalog!;
 		
-		s!KCmdLineArgs::addStdCmdLineOptions!KCmdLineArgs::addStdCmdLineOptions!;
+		s!KApplication::addCmdLineOptions!KCmdLineArgs::addStdCmdLineOptions!;
 		
-		if ( /KAuthorized::authorizeKAction/ ) {
-			s!KAuthorized::authorizeKAction!KAuthorized::authorizeKAction!;
+		if ( /kapp->authorizeKAction/ ) {
+			s!kapp->authorizeKAction!KAuthorized::authorizeKAction!;
 			$necessaryToAddIncludeAuthorize = 1;
 	}
-		if ( /KAuthorized::authorizeKAction/ ) {
-			s!KAuthorized::authorizeKAction!KAuthorized::authorizeKAction!;
+		if ( /kapp->authorize/ ) {
+			s!kapp->authorize!KAuthorized::authorizeKAction!;
 			$necessaryToAddIncludeAuthorize = 1;
 		}
-		if ( /KToolInvocation::startServiceByDesktopName/ ) {
-			s!KToolInvocation::startServiceByDesktopName!KToolInvocation::startServiceByDesktopName!;
+		if ( /KApplication::startServiceByDesktopName/ ) {
+			s!KApplication::startServiceByDesktopName!KToolInvocation::startServiceByDesktopName!;
 			$necessaryToAddIncludektoolinvocation = 1;
 		}
-		if ( /KToolInvocation::startServiceByDesktopName/ ) {
-		        s!KToolInvocation::startServiceByDesktopName!KToolInvocation::startServiceByDesktopName!;
+		if ( /kapp->startServiceByDesktopName/ ) {
+		        s!kapp->startServiceByDesktopName!KToolInvocation::startServiceByDesktopName!;
 				$necessaryToAddIncludektoolinvocation = 1;
 		}
-		if ( /KToolInvocation::kdeinitExec/ ) {
-				s!KToolInvocation::kdeinitExec!KToolInvocation::kdeinitExec!;
+		if ( /kapp->kdeinitExec/ ) {
+				s!kapp->kdeinitExec!KToolInvocation::kdeinitExec!;
 				$necessaryToAddIncludektoolinvocation = 1;
 		}
-		if ( /KToolInvocation::invokeHelp/ ) {
-				s!KToolInvocation::invokeHelp!KToolInvocation::invokeHelp!;
+		if ( /kapp->invokeHelp/ ) {
+				s!kapp->invokeHelp!KToolInvocation::invokeHelp!;
 				$necessaryToAddIncludektoolinvocation = 1;
 		}
-		if ( /KToolInvocation::invokeMailer/ ) {
-				s!KToolInvocation::invokeMailer!KToolInvocation::invokeMailer!;
+		if ( /kapp->invokeMailer/ ) {
+				s!kapp->invokeMailer!KToolInvocation::invokeMailer!;
 				$necessaryToAddIncludektoolinvocation = 1;
 		}
-        if ( /KToolInvocation::invokeBrowser/ ) {
-                s!KToolInvocation::invokeBrowser!KToolInvocation::invokeBrowser!;
+        if ( /kapp->invokeBrowser/ ) {
+                s!kapp->invokeBrowser!KToolInvocation::invokeBrowser!;
                 $necessaryToAddIncludektoolinvocation = 1;
         }
-        if ( /KToolInvocation::kdeinitExecWait/ ) {
-                s!KToolInvocation::kdeinitExecWait!KToolInvocation::kdeinitExecWait!;
+        if ( /kapp->kdeinitExecWait/ ) {
+                s!kapp->kdeinitExecWait!KToolInvocation::kdeinitExecWait!;
                 $necessaryToAddIncludektoolinvocation = 1;
         }
-		if ( /KToolInvocation::startServiceByDesktopPath/ ) {
-				s!KToolInvocation::startServiceByDesktopPath!KToolInvocation::startServiceByDesktopPath!;
+		if ( /KApplication::startServiceByDesktopPath/ ) {
+				s!KApplication::startServiceByDesktopPath!KToolInvocation::startServiceByDesktopPath!;
 				$necessaryToAddIncludektoolinvocation = 1;
 		}
-		if ( /KToolInvocation::startServiceByName/ ) {
-				s!KToolInvocation::startServiceByName!KToolInvocation::startServiceByName!;
+		if ( /KApplication::startServiceByName/ ) {
+				s!KApplication::startServiceByName!KToolInvocation::startServiceByName!;
 				$necessaryToAddIncludektoolinvocation = 1;
 		}
-        if ( /KToolInvocation::startServiceByName/ ) {
-                s!KToolInvocation::startServiceByName!KToolInvocation::startServiceByName!;
+        if ( /kapp->startServiceByName/ ) {
+                s!kapp->startServiceByName!KToolInvocation::startServiceByName!;
                 $necessaryToAddIncludektoolinvocation = 1;
         }	
 		if ( /new KRun/ ) {
 				$warning = $warning . "Be carrefull perhaps necessary to add parent into constructor in file : $file\n";
 		}
-		s!KLibLoader::createInstance!KLibLoader::createInstance!;
+		s!KParts::ComponentFactory::createInstanceFromLibrary!KLibLoader::createInstance!;
 	    #KMainWindow
-	    s/(?<!KMainWindow::memberList()\(\))KMainWindow::memberList/KMainWindow::memberList()/;	
-		#s!KMainWindow::memberList()!KMainWindow::memberList()!;
+	    s/(?<!KMainWindow::memberList\(\))KMainWindow::memberList/KMainWindow::memberList()/;	
+		#s!KMainWindow::memberList!KMainWindow::memberList()!;
 	    if ( /kapp->getDisplay/ ) {
 			s!kapp->getDisplay\s*\(\s*\)!QX11Info::display()!;
 			$necessaryToAddInclude = 1;
