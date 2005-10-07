4d5dd2d6b723457ac1eb46480f161e19660a38c7
Laurent Montel <montel@kde.org>
2005-10-07 15:25:57 +0000

    Adapt script
    svn path=/trunk/KDE/kdesdk/scripts/; revision=468282

--- qt4/remove-qt3-support.pl
+++ qt4/remove-qt3-support.pl
@@ -8,86 +8,99 @@ use lib dirname( $0 );
 use functionUtilkde;
 use strict;
 
-foreach my $file (@ARGV) {
-    functionUtilkde::substInFile {
-			if ( $_ =~ /findRev/ ) {
-				# three argument
-				my ($before, $prefix, $contenu) = m!^(\s*.*)(findRev.*)\((.*)\s*\);$!;
-				if ( my ($firstelement, $secondelement, $thirdelement) = m!.*?\(\s*(.*),\s*(.*),\s*(.*)\);\s*$!) {
-						$thirdelement =~ s/ //g;
-						if ( $thirdelement =~ /true/ ) {
-								$_ = $before . "lastIndexOf( $firstelement, $secondelement, Qt::CaseSensitive );\n";
-						}
-						elsif ( $thirdelement =~ /false/ ) {
-								$_ = $before . "lastIndexOf( $firstelement, $secondelement, Qt::CaseInsensitive );\n";
-						}
-						else {
-								s/findRev/lastIndexOf/;
-						}
-				}
-				else {
-					s/findRev/lastIndexOf/;	
-				}
-			}	
-			s!local8Bit!toLocal8Bit!g;
-			if( $_ =~ /\.utf8/ ) {
-				s!\.utf8!\.toUtf8!g;
-			}	
-			if( $_ =~ /\.mirrored/ ) {
-				s!mirrored!hasMirrored!g;
-			}
-			if( $_ =~ /absPath\s*\(\s*\)/ ) {
-					s!absPath!absolutePath!;
-			}
-			if ( $_ =~ /\.lower\s*\(\s*\)/ ) {
-					s!\.lower!\.toLower!g;
-			}
+open(my $F, q(find -name "*" |));
+my $file;
+while ($file = <$F>) {
+ 	chomp $file;
+	next if functionUtilkde::excludeFile( $file);
+	open(my $FILE, $file) or warn "We can't open file $file:$!\n";
+	my $modified;
+	my @l = map {
+	    my $orig = $_;
+	    if ( $_ =~ /lastIndexOf/ ) {
+		# three argument
+		my ($before, $prefix, $contenu) = m!^(\s*.*)(lastIndexOf.*)\((.*)\s*\);$!;
+		if ( my ($firstelement, $secondelement, $thirdelement) = m!.*?\(\s*(.*),\s*(.*),\s*(.*)\);\s*$!) {
+		    $thirdelement =~ s/ //g;
+		    if ( $thirdelement =~ /true/ ) {
+			$_ = $before . "lastIndexOf( $firstelement, $secondelement, Qt::CaseSensitive );\n";
+		    }
+		    elsif ( $thirdelement =~ /false/ ) {
+			$_ = $before . "lastIndexOf( $firstelement, $secondelement, Qt::CaseInsensitive );\n";
+		    }
+		    else {
+			s/lastIndexOf/lastIndexOf/;
+		    }
+		}
+		else {
+		    s/lastIndexOf/lastIndexOf/;	
+		}
+	    }	
+	    s!toLocal8Bit!toLocal8Bit!g;
+	    if( $_ =~ /\.toUtf8/ ) {
+		s!\.toUtf8!\.toUtf8!g;
+	    }	
+	    if( $_ =~ /\.hasMirrored/ ) {
+		s!mirrored!hasMirrored!g;
+	    }
+	    if( $_ =~ /absPath\s*\(\s*\)/ ) {
+		s!absPath!absolutePath!;
+	    }
+	    if ( $_ =~ /\.lower\s*\(\s*\)/ ) {
+		s!\.lower!\.toLower!g;
+	    }
             if ( $_ =~ /\.upper\s*\(\s*\)/ ) {
-                    s!\.upper!\.toUpper!g;
+		s!\.upper!\.toUpper!g;
             }			
-			if( $_ =~ /dirPath/ ) {
-					if( my ($before, $prefix, $contenu, $after ) = m!^(\s*.*)(dirPath.*?\()(.*?\))(.*)$!) {
-							$contenu =~ s/ //g;
-							$contenu =~ s/\)//;
-							if( $contenu =~ /true|TRUE/ ) {
-									$_ = $before . "absolutePath()" . $after . "\n";
-							}
-							elsif ( $contenu =~ /false|FALSE/ ) {
-									$_ = $before . "path()" . $after . "\n";
-							}
-							elsif ($contenu eq "" ) {
-									$_ = $before . "path()" . $after . "\n";
-							}
-							else {
-									warn "Verify if we can port or not : <$contenu> \n";
-							}
-					}	
-			}
-			s!Qt::ShiftButton!Qt::ShiftModifier!;
-			s!Qt::ControlButton!Qt::ControlModifier!;
-			s!Qt::AltButton!Qt::AltModifier!;
-			s!Qt::MetaButton!Qt::MetaModifier!;
-			s!Qt::Keypad!Qt::KeypadModifier!;
-			s!Qt::KeyButtonMask!Qt::KeyboardModifierMask!;
-			s!convertToAbs!makeAbsolute!;
-			s!currentDirPath!currentPath!;
-			s!homeDirPath!homePath!;
-			s!rootDirPath!rootPath!;
-			s!cleanDirPath!cleanPath!;
-			s!absFilePath!absoluteFilePath!;
-			s!QDir::All!QDir::TypeMask!;
-			s!QDir::DefaultFilter!QDir::NoFilter!;
-			s!QDir::DefaultSort!QDir::NoSort!;
-			s!simplifyWhiteSpace!simplified!g;
-			s!stripWhiteSpace!trimmed!g;
-			s!ucs2!utf16!g;
-			s!leftJustify!leftJustified!g;
-			s!rightJustify!rightJustified!g;
-			s!fromUcs2!fromUtf16!g;
-			s!constref!at!g;
-			s!changeInterval!start!g;
-    } $file;
+	    if( $_ =~ /dirPath/ ) {
+		if( my ($before, $prefix, $contenu, $after ) = m!^(\s*.*)(dirPath.*?\()(.*?\))(.*)$!) {
+		    $contenu =~ s/ //g;
+		    $contenu =~ s/\)//;
+		    if( $contenu =~ /true|TRUE/ ) {
+			$_ = $before . "absolutePath()" . $after . "\n";
+		    }
+		    elsif ( $contenu =~ /false|FALSE/ ) {
+			$_ = $before . "path()" . $after . "\n";
+		    }
+		    elsif ($contenu eq "" ) {
+			$_ = $before . "path()" . $after . "\n";
+		    }
+		    else {
+			warn "Verify if we can port or not : <$contenu> \n";
+		    }
+		}	
+	    }
+	    s!Qt::ShiftModifier!Qt::ShiftModifier!;
+	    s!Qt::ControlModifier!Qt::ControlModifier!;
+	    s!Qt::AltModifier!Qt::AltModifier!;
+	    s!Qt::MetaModifier!Qt::MetaModifier!;
+	    s!Qt::KeypadModifier!Qt::KeypadModifier!;
+	    s!Qt::KeyboardModifierMask!Qt::KeyboardModifierMask!;
+	    s!makeAbsolute!makeAbsolute!;
+	    s!currentPath!currentPath!;
+	    s!homePath!homePath!;
+	    s!rootPath!rootPath!;
+	    s!cleanPath!cleanPath!;
+	    s!absoluteFilePath!absoluteFilePath!;
+	    s!QDir::TypeMask!QDir::TypeMask!;
+	    s!QDir::NoFilter!QDir::NoFilter!;
+	    s!QDir::NoSort!QDir::NoSort!;
+	    s!simplified!simplified!g;
+	    s!trimmed!trimmed!g;
+	    s!utf16!utf16!g;
+	    s!leftJustified!leftJustified!g;
+	    s!rightJustified!rightJustified!g;
+	    s!fromUtf16!fromUtf16!g;
+	    s!at!at!g;
+	    s!start!start!g;
+	    $modified ||= $orig ne $_;
+	    $_;
+	} <$FILE>;
+	if ($modified) {
+	    open (my $OUT, ">$file");
+	    print $OUT @l;
+	}
 
-}
+    }
 functionUtilkde::diffFile( "@ARGV" );
 
--- qt4/search-remove-qt3-support.pl
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-egrep -rl 'findRev|local8Bit|utf8|simplifyWhiteSpace|stripWhiteSpace|ucs2|leftJustify|rightJustify|fromUcs2|constref|mirrored|changeInterval|absPath|convertToAbs|currentDirPath|homeDirPath|rootDirPath|cleanDirPath|absFilePath|dirPath|ShiftButton|ControlButton|AltButton|MetaButton|Keypad|KeyButtonMask|\.lower|\.upper' * |grep -v "\.svn" |grep -v "\.libs" | grep -v "\.o" | grep -v Makefile | grep -v Makefile.in  | grep -v "\.moc" | grep -v "\.lo" | grep -v "\.la" |grep -v "\.Ulo" |grep -v "\.kidl" | grep -v "\.desktop"
