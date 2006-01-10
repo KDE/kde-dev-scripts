3f5869eb5a7252729ac75aa176cbc8b925d16792
Laurent Montel <montel@kde.org>
2006-01-10 13:52:44 +0000

    writePathEntry must be modify too
    svn path=/trunk/KDE/kdesdk/scripts/; revision=496445

--- qt4/adapt-to-new-kconfig2.pl
+++ qt4/adapt-to-new-kconfig2.pl
@@ -20,6 +20,53 @@ while ($file = <$F>) {
     open(my $FILE, $file) or warn "We can't open file $file:$!\n";
     my @l = map {
 	my $orig = $_;
+
+	# writePathEntry change API
+	if (my ($prefix, $parenthese, $end) = /(.*writePathEntry\s*\()(.*)(\).*)/) {
+	    warn "prefix !!!! :<$prefix>  parenthese <$parenthese> end <$end>\n";	
+	    my $changes = $end;
+	    $changes =~ s/\)//;
+	    if ( my ($firstelement, $secondelement,$thirdelement,$fouthelement,$fifthelement) = $parenthese =~ m!(.*),\s*(.*),\s*(.*),\s*(.*),\*(.*)$!) {
+		warn "5 elements : firstelement <$firstelement>  secondelement <$secondelement> thirdelement<$thirdelement> fouthelement <$fouthelement> fifthelement <fifthelement> \n";
+		if ( $thirdelement =~ /(true|TRUE)/ && $fouthelement =~ /(true|TRUE)/ && $fifthelement =~ /(true|TRUE)/  )
+		{
+		    $_ = $prefix . "$firstelement, $secondelement, KConfigBase::Normal|KConfigBase::Global|KConfigBase::NLS" .  $end . "\n";
+	        }
+		elsif ( $thirdelement =~ /(false|FALSE)/ && $fouthelement =~ /(true|TRUE)/ && $fifthelement =~ /(true|TRUE)/  )
+		{
+		    $_ = $prefix . "$firstelement, $secondelement, KConfigBase::Global|KConfigBase::NLS" .  $end . "\n";
+		}
+		elsif ( $thirdelement =~ /(true|TRUE)/ && $fouthelement =~ /(false|FALSE)/ && $fifthelement =~ /(true|TRUE)/ )
+		{
+		    $_ = $prefix . "$firstelement, $secondelement, KConfigBase::Normal|KConfigBase::NLS" .  $end . "\n";
+		}
+		
+	    }
+	    elsif ( my ($firstelement, $secondelement,$thirdelement,$fouthelement) = $parenthese =~ m!(.*),\s*(.*),\s*(.*),\s*(.*)$!) {
+		warn "4 elements : firstelement <$firstelement>  secondelement <$secondelement> thirdelement<$thirdelement> fouthelement <$fouthelement> \n";
+		if ( $thirdelement =~ /(true|TRUE)/ && $fouthelement =~ /(true|TRUE)/ )
+		{
+		    $_ = $prefix . "$firstelement, $secondelement, KConfigBase::Normal|KConfigBase::Global" .  $end . "\n";
+	        }
+		elsif ( $thirdelement =~ /(false|FALSE)/ && $fouthelement =~ /(true|TRUE)/ )
+		{
+		    $_ = $prefix . "$firstelement, $secondelement, KConfigBase::Global" .  $end . "\n";
+		}
+		elsif ( $thirdelement =~ /(true|TRUE)/ && $fouthelement =~ /(false|FALSE)/ )
+		{
+		    $_ = $prefix . "$firstelement, $secondelement, KConfigBase::Normal" .  $end . "\n";
+		}
+		
+	    }
+	    elsif ( my ($firstelement, $secondelement,$thirdelement) = $parenthese =~ m!(.*),\s*(.*),\s*(.*)$!) {
+		warn " 3 elements :  firstelement <$firstelement>  secondelement <$secondelement> thirdelement<$thirdelement>\n";
+		if ( $thirdelement =~ /(true|TRUE)/ )
+		{
+		    $_ = $prefix . "$firstelement, $secondelement, KConfigBase::Normal" .  $end . "\n";
+		}
+	    }
+	}	
+
 	# deleteGroup Change API
 	if (my ($prefix, $parenthese, $end) = /(.*deleteGroup\s*\()(.*)(\).*)/) {
 	    warn "prefix !!!! :<$prefix>  parenthese <$parenthese> end <$end>\n";	
