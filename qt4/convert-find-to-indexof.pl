afd6b7c347db8b226f51bd9bed999bd460361402
Reinhold Kainhofer <reinhold@kainhofer.com>
2006-04-28 18:20:05 +0000

    Simple scripts to convert QLabel constructors and QList::find / QString::find. A lot of manual intervention (i.e. looking at the diffs and reverting whole files) is needed.
    svn path=/trunk/KDE/kdesdk/scripts/; revision=535172

---
+++ qt4/convert-find-to-indexof.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+
+# laurent Montel <montel@kde.org> 2006 GPL
+# Reinhold Kainhofer <reinhold@kainhofer.com> 2006 GPL
+# 
+# simple script to replace find by indexOf
+#
+# usage:
+#    convert-find-to-indexof.pl *.cpp
+#
+# CAUTION: This script replaces all calls to methods named 'find', even if the 
+# object is not a class where the find method is deprecated. 
+# I apply the script only to files that 
+# throw a deprecated warning and compare the # of deprecated warnings to the nr 
+# of replacements.
+#
+# NOTE: The results need a lot of manual tweaking!!!!
+
+
+use File::Basename;
+use lib dirname( $0 );
+use functionUtilkde;
+use strict;
+
+foreach my $file (@ARGV) {
+    chomp $file;
+    next if functionUtilkde::excludeFile( $file);
+
+    my $modified;
+    my @necessaryIncludes = ();
+    open(my $FILE, $file) or warn "We can't open file $file:$!\n";
+    my @l = map {
+      my $orig = $_;
+      if ( m![\.>]find\s*\(! ) {
+#       s/(.*\.)find/!XXX-\1-XXXYYY-\2-YYY/g;
+#       s/(.*\.)find(\(.*\)\s*)==\s*([.*]\.)end\s*\(\s*\)/!\1contains\2/g;
+        s/([^ ].*(.|->))find(\(.*\)\s*)==\s*\1end\s*\(\s*\)/!\1contains\3/g;
+        s/([^ ]*(.|->))find(\(.*\)\s*)==\s*-1\s*/!\1contains\3/g;
+        s/([^ ].*(.|->))find(\(.*\)\s*)!=\s*\1end\s*\(\s*\)/\1contains\3/g;
+        s/([^ ]*(.|->))find(\(.*\)\s*)>=\s*0\s*/\1contains\3/g;
+        s/([^ ]*(.|->))find(\(.*\)\s*)!=\s*-1\s*/\1contains\3/g;
+# print "Old line:      \n$orig";
+# print "New line:      \n$_====================\n";
+      }
+      
+      if ( m![\.>]find\s*\(! && ! m/[mM]ap/ ) {
+        s/([\.>])find(\s*)\(/\1indexOf\2(/g;
+      }
+      # third argument of find was changed from bool to  Qt::CaseSensitivity cs = Qt::CaseSensitive
+      if ( my ($head, $parm1, $parm2, $ws, $parm3,$trailer) = m!^(.*ndexOf\s*)\(([^,]*),([^,]*),(\s*)([^, \)]*)(\s*\).*$)! ) {
+        if ( $parm3 eq 'true' ) {
+          $_ = "$head($parm1,$parm2,$ws"."Qt::CaseSensitive$trailer\n";
+        } elsif ( $parm3 eq 'false' ) {
+          $_ = "$head($parm1,$parm2,$ws"."Qt::CaseInsensitive$trailer\n";
+        }
+#if ( $orig ne $_ ) {
+#  print "Old line:      \n$orig";
+#  print "New line:      \n$_====================\n";
+#}
+      }
+      $modified ||= $orig ne $_;
+      $_;
+    } <$FILE>;
+
+    if ($modified) {
+      print "Modified: $file\n";
+      open (my $OUT, ">$file");
+      print $OUT @l;
+      close $OUT;
+    }
+
+}
+functionUtilkde::diffFile( @ARGV );
+
---
+++ qt4/convert-qlabel.pl
@@ -0,0 +1,129 @@
+#!/usr/bin/perl
+
+# laurent Montel <montel@kde.org> 2006 GPL
+# Reinhold Kainhofer <reinhold@kainhofer.com> 2006 GPL
+# 
+# Heuristically try to detect Q*Layout constructors where the parent is a layout.
+# Whenever the parent matches .*[lL]ayout.*, assume the parent is a QLayout* and
+# port the constructor to the new API.
+#
+# NOTE: This script assumes that the QGridLayout and Q[HV]BoxLayout constructors
+#       are already converted to the new API, except for the possible
+#       QLayout*parent first argument. Use the convert-qgridlayout.pl and
+#       convert-qboxlayout.pl scripts for this.
+
+use File::Basename;
+use lib dirname( $0 );
+use functionUtilkde;
+use strict;
+
+open(my $F, q(find [ac-z]* -name "*" | grep -v '\.svn/' |));
+my $file;
+while ($file = <$F>) {
+    chomp $file;
+    next if functionUtilkde::excludeFile( $file);
+
+    my $modified;
+    my @necessaryIncludes = ();
+    open(my $FILE, $file) or warn "We can't open file $file:$!\n";
+    my @l = map {
+      my $orig = $_;
+      my ($spaces, $trailer, $object, $call, $ws, $params );
+      if ( ($spaces, $trailer, $object, $call, $ws, $params ) = m!^(\s*)(.*[\s\*]|)([a-zA-Z0-9_]+)(\s*=\s*new QLabel\s*)\((\s*)(.*[^\s])\s*\);$! ) {
+# print "Spaces: '$spaces', Trailer: '$trailer', Object: '$object', Call: '$call', WS: '$ws', Params: '$params'\n";
+        my @parms = split( /,\s*/, $params );
+        
+        my $buddy, my $text, my $parent, my $name, my $flags;
+
+print "Case ";
+        if ( $parms[0] =~ m/i18n\(.*/ ) {
+print "1";
+          $text=$parms[0];
+          $parent=$parms[1];
+          $name=$parms[2] if scalar(@parms)>2;
+          $flags=$parms[3] if scalar(@parms)>3;
+        } elsif ( scalar(@parms) == 5 ) {
+          ($buddy, $text, $parent, $name, $flags) = @parms;
+print "2";
+        
+        } elsif (scalar(@parms) == 4 ) {
+          if ( $parms[3]=~m/Qt::/ ){
+            ($text, $parent, $name, $flags) = @parms;
+           } else {
+             ($buddy, $text, $parent, $name) = @parms;
+           }
+print "3";
+        
+        } elsif (scalar(@parms) == 3) {
+          if ( $parms[2]=~m/Qt::/ ){
+            ($parent, $name, $flags) = @parms;
+          } elsif ( $parms[2]=~m/^".*"$/ ) {
+            ($text, $parent, $name) = @parms;
+          } else {
+            ($buddy, $text, $parent) = @parms;
+          }
+print "4";
+        } elsif (scalar(@parms) == 2) {
+          if ( $parms[1] =~ m/this/ ) {
+            ($text, $parent) = @parms;
+          } elsif ( $parms[1] =~ m/^".*"$/ ) {
+            ($parent, $name) = @parms;
+          } else {
+            ($buddy, $text) = @parms;
+          }
+print "5";
+        } elsif ( scalar(@parms) == 1 ) {
+          $parent = $parms[0];
+print "6";
+        }
+print "\n";
+        
+        $_ = "$spaces$trailer$object$call(";
+        my $p="";
+        $p.= "$text" if ( defined($text) );
+        if ( defined($parent) ) {
+          $p.= ",$ws" if ( defined($text) );
+          $p.= "$parent";
+          if ( defined($flags) ) { 
+            $p.=",$ws$flags";
+          }
+        }
+        if (defined($p)) { $_.="$ws$p$ws"; }
+        $_.=");\n";
+        if ( defined($name) ) {
+          $_.="$spaces$object->setObjectName($ws$name$ws);\n";
+        }
+        if ( defined($buddy) ) {
+          $_.="$spaces$object->setBuddy($ws$buddy$ws);\n";
+        }
+print "ORIG: $orig";
+print "NEW:  $_";
+# print "Original call: $call($ws$params$ws)\n";
+print "Buddy: $buddy, Text: $text, Parent: $parent, Name: $name, Flags: $flags\n";
+        
+        
+# QT4 API:
+# QLabel ( QWidget * parent = 0, Qt::WFlags f = 0 ) 
+# QLabel ( const QString & text, QWidget * parent = 0, Qt::WFlags f = 0 ) 
+# 
+#         
+# DEPRECATED:
+# QLabel ( QWidget * parent, const char * name, Qt::WFlags f = 0 ) 
+# QLabel ( const QString & text, QWidget * parent, const char * name, Qt::WFlags f = 0 ) 
+# QLabel ( QWidget * buddy, const QString & text, QWidget * parent = 0, const char * name = 0, Qt::WFlags f = 0 ) 
+
+      }
+      $modified ||= $orig ne $_;
+      $_;
+    } <$FILE>;
+
+    if ($modified) {
+      print "Modified: $file\n";
+      open (my $OUT, ">$file");
+      print $OUT @l;
+      close $OUT;
+    }
+
+}
+functionUtilkde::diffFile( <$F> );
+
