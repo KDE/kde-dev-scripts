eb2ab2e31316edcadf01d13eca9e27c6b592799b
Reinhold Kainhofer <reinhold@kainhofer.com>
2006-04-28 18:18:55 +0000

    Allow replacing QWhatsThis::add, too
    svn path=/trunk/KDE/kdesdk/scripts/; revision=535170

--- qt4/convert-whatsthis.pl
+++ qt4/convert-whatsthis.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl -pi
 
-s!Q3WhatsThis::add\s*\(\s*([^,]*),!\1->setWhatsThis(!;
-s!#include <q3whatsthis.h>!!;
+s!Q(3|)WhatsThis::add\s*\(\s*([^,]*),!\2->setWhatsThis(!g;
+s!#include <q3whatsthis.h>!!g;
 
 # Q(V|H)GroupBox convertion
 # TODO add #include <Q3GroupBox>
-s!new\s*QVGroupBox\s*\(!new Q3GroupBox\(1, Qt::Horizontal,!;
-s!new\s*QHGroupBox\s*\(!new Q3GroupBox\(1, Qt::Vertical,!;
-s!QVGroupBox\s*\*!Q3GroupBox *!;
-s!#include <qvgroupbox.h>!!;
+s!new\s*QVGroupBox\s*\(!new Q3GroupBox\(1, Qt::Horizontal,!g;
+s!new\s*QHGroupBox\s*\(!new Q3GroupBox\(1, Qt::Vertical,!g;
+s!QVGroupBox\s*\*!Q3GroupBox *!g;
+s!#include <qvgroupbox.h>!!g;
