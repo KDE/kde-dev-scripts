e52885c4f5b9b6d914de4d5e4133c92890fc2aa3
Stephan Kulow <coolo@kde.org>
2006-05-04 09:58:08 +0000

    the heurstics didn't work, but greping a compile log for a specific warning and then calling the script turned out to be pretty reliable :)
    svn path=/trunk/KDE/kdesdk/scripts/; revision=537265

--- qt4/convert-qlayoutparent.pl
+++ qt4/convert-qlayoutparent.pl
@@ -3,10 +3,8 @@
 # laurent Montel <montel@kde.org> 2006 GPL
 # Reinhold Kainhofer <reinhold@kainhofer.com> 2006 GPL
 # 
-# Heuristically try to detect Q*Layout constructors where the parent is a layout.
-# Whenever the parent matches .*[lL]ayout.*, assume the parent is a QLayout* and
-# port the constructor to the new API.
-#
+# Call with a file and a line number to change (by grepping compile logs)
+
 # NOTE: This script assumes that the QGridLayout and Q[HV]BoxLayout constructors
 #       are already converted to the new API, except for the possible
 #       QLayout*parent first argument. Use the convert-qgridlayout.pl and
@@ -17,36 +15,25 @@ use lib dirname( $0 );
 use functionUtilkde;
 use strict;
 
-open(my $F, q(find -name "*" | grep -v '\.svn/' |));
-my $file;
-while ($file = <$F>) {
-    chomp $file;
-    next if functionUtilkde::excludeFile( $file);
+my $file = $ARGV[0];
+my $cline = $ARGV[1];
 
-    my $modified;
-    my @necessaryIncludes = ();
-    open(my $FILE, $file) or warn "We can't open file $file:$!\n";
-    my @l = map {
-      my $orig = $_;
-      my ($spaces, $trailer, $object, $call, $ws, $parent );
-      if ( ($spaces, $trailer, $object, $call, $ws, $parent ) = m!^(\s*)(.*[\s\*]|)([a-zA-Z0-9]+)(\s*=\s*new Q[a-zA-Z0-9]*Layout[^(]*)\((\s*)(.*[^\s])\s*\);$! ) {
-print "Spaces: '$spaces', Trailer: '$trailer', Object: '$object', Call: '$call', WS: '$ws', Parent: '$parent'\n";
-        if ( $parent =~ m![lL]ayout! && $parent !~ m![wW]idget! ) {
+my $line = 0;
+
+my $modified;
+open(FILE, $file) or warn "We can't open file $file:$!\n";
+while ( <FILE> )
+{
+   $line = $line + 1;
+   if ($line == $cline) {
+           my ($spaces, $trailer, $object, $call, $ws, $parent );
+           if ( ($spaces, $trailer, $object, $call, $ws, $parent ) = m!^(\s*)(.*[\s\*]|)([a-zA-Z0-9]+)(\s*=\s*new Q[a-zA-Z0-9]*Layout[^(]*)\((\s*)(.*[^\s])\s*\);$! ) {
+#print "Spaces: '$spaces', Trailer: '$trailer', Object: '$object', Call: '$call', WS: '$ws', Parent: '$parent'\n";
           $_  = "$spaces$trailer$object$call();\n";
           $_ .= "$spaces$parent->addItem($ws$object$ws);\n";
-        }
       }
-      $modified ||= $orig ne $_;
-      $_;
-    } <$FILE>;
 
-    if ($modified) {
-      print "Modified: $file\n";
-      open (my $OUT, ">$file");
-      print $OUT @l;
-      close $OUT;
-    }
-
+   }
+   print $_;
 }
-functionUtilkde::diffFile( <$F> );
 
