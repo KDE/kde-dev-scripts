806509946ec054986f21656b1ccd8c80df147f69
Matthias Kretz <kretz@kde.org>
2007-01-24 22:38:08 +0000

    a lot more good conversions and less wrong changes
    svn path=/trunk/KDE/kdesdk/scripts/; revision=626909

--- qt4/convert-kinstance.pl
+++ qt4/convert-kinstance.pl
@@ -24,26 +24,49 @@ while ($file = <$F>) {
 
         s!#include <kinstance.h>!#include <kcomponentdata.h>!;
         s!#include "kinstance.h"!#include "kcomponentdata.h"!;
-        s!kapp->instanceName\s*\(!KGlobal::mainComponent().componentName\(!g;
-        s!KInstance::caption\s*\(!KGlobal::caption\(!g;
+        s!kapp->instanceName\s?\(!KGlobal::mainComponent().componentName(!g;
+        s!KInstance::caption\s?\(!KGlobal::caption(!g;
+        s!kapp->caption\s?\(!KGlobal::caption(!g;
         s!const\s*KInstance\s*\*\s*!const KComponentData &!g;
         s!KInstance\s*\*\s*!KComponentData !g;
-        s!KConfig\s*\*\s*([a-zA-Z0-9_])\s*=\s*KGlobal::config\s*\(!KSharedConfig::Ptr \1 = KGlobal::config\(!g;
-        s!KConfig\s*\*\s*([a-zA-Z0-9_])\s*=\s*KGlobal::instance\s*()->config\s*\(!KSharedConfig::Ptr \1 = KGlobal::config\(!g;
-        s!KGlobal::instance\s*\(\)->aboutData\s*\(!KGlobal::aboutData\(!g;
-        s!KGlobal::instance\s*\(!KGlobal::mainComponent\(!g;
-        s!KGlobal::activeInstance\s*\(!KGlobal::activeComponent\(!g;
-        s!KGlobal::sharedConfig\s*\(!KGlobal::config\(!g;
-        s!->sharedConfig\s*\(!.config\(!g;
-        s!->instanceName\s*\(!.componentName\(!g;
-        s!\s*=\s*new KInstance\s*\(!\(!g;
-        s!new KInstance\s*\(!KComponentData\(!g;
+        s!KConfig\s*\*\s*([a-zA-Z0-9_]+)\s*=\s*kapp->config\s?\(!KSharedConfig::Ptr \1 = KGlobal::config(!g;
+        s!KConfig\s*\*\s*([a-zA-Z0-9_]+)\s*=\s*KGlobal::config\s?\(!KSharedConfig::Ptr \1 = KGlobal::config(!g;
+        s!KConfig\s*\*\s*([a-zA-Z0-9_]+)\s*=\s*KGlobal::instance\s?()->config\s*\(!KSharedConfig::Ptr \1 = KGlobal::config(!g;
+        s!KGlobal::instance\s?\(\)->!KGlobal::mainComponent().!g;
+        s!KGlobal::instance\s?\(!KGlobal::mainComponent(!g;
+        s!KGlobal::activeInstance\s?\(!KGlobal::activeComponent(!g;
+        s!KGlobal::setActiveInstance\s?\(!KGlobal::setActiveComponent(!g;
+        s!KGlobal::sharedConfig\s?\(!KGlobal::config(!g;
+        s!->sharedConfig\s?\(!.config(!g;
+        s!->instanceName\s?\(!.componentName(!g;
+        s!\s*=\s*new KInstance\s*\(!(!g;
+        s!new KInstance\s?\(!KComponentData(!g;
         s!KInstance!KComponentData!g;
         # the following might be a little too bold:
-        s!\binstance\b!componentData!g;
+        s!KConfig\s*\*\s*([a-zA-Z0-9_]+)\s*=\s*instance->config\s?\(!KSharedConfig::Ptr \1 = componentData.config(!g;
+        s!KConfig\s*\*\s*([a-zA-Z0-9_]+)\s*=\s*instance\(\)->config\s?\(!KSharedConfig::Ptr \1 = componentData().config(!g;
+        s!KConfig\s*\*\s*([a-zA-Z0-9_]+)\s*=\s*_instance->config\s?\(!KSharedConfig::Ptr \1 = _componentData.config(!g;
+        s!KConfig\s*\*\s*([a-zA-Z0-9_]+)\s*=\s*m_instance->config\s?\(!KSharedConfig::Ptr \1 = m_componentData.config(!g;
+        s!KConfig\s*\*\s*([a-zA-Z0-9_]+)\s*=\s*mInstance->config\s?\(!KSharedConfig::Ptr \1 = mComponentData.config(!g;
+        s!instanceNames\s?\(!componentNames(!g;
+        s!\binstance->!componentData.!g;
+        s!\b_instance->!_componentData.!g;
+        s!\bm_instance->!m_componentData.!g;
+        s!\bmInstance->!mComponentData.!g;
+        s!\binstance\s?\(\)->!componentData().!g;
+        s!\binstance\s?\(!componentData(!g;
+        s!\bpartInstance\s?\(!partComponentData(!g;
+        s!\bcreateInstance\s?\(!createComponentData(!g;
+        s!QCoreApplication::componentData\s?\(\)\.!QCoreApplication::instance()->!g;
+        s!QApplication::componentData\s?\(\)\.!QApplication::instance()->!g;
+        s!QCoreApplication::componentData\s?\(!QCoreApplication::instance(!g;
+        s!QApplication::componentData\s?\(!QApplication::instance(!g;
+        #s!\binstance\b!componentData!g;
         s!\b_instance\b!_componentData!g;
         s!\bm_instance\b!m_componentData!g;
         s!\bmInstance\b!mComponentData!g;
+        s!\bsetInstance\b!setComponentData!g;
+        s!\binstanceName\b!componentName!g;
 
         $modified ||= $orig ne $_;
         $_;
@@ -55,3 +78,4 @@ while ($file = <$F>) {
     }
 }
 functionUtilkde::diffFile( "@ARGV" );
+# vim: et
