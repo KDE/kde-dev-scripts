4e05b5b4db21a14fb1218bfc5aea2ed404bc0792
David Faure <faure@kde.org>
2007-03-16 12:13:20 +0000

    Same for *PixelSqueeze
    svn path=/trunk/KDE/kdesdk/scripts/; revision=643113

--- qt4/convert-kstringhandler.pl
+++ qt4/convert-kstringhandler.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 # David Faure <faure@kde.org>
-# This script converts KStringHandler::*EmSqueeze to QFontMetrics::elidedText
+# This script converts KStringHandler::*EmSqueeze/*PixelSqueeze to QFontMetrics::elidedText
 # Based on convert-qstringlist.
 
 use lib qw( . );
@@ -12,6 +12,11 @@ foreach my $file (@ARGV) {
         s!KStringHandler::lEmSqueeze\s*\(\s*(.+)\s*,\s*(.+)\s*,\s*(.+)\s*\)!$2.elidedText( $1, Qt::ElideLeft, $2.maxWidth() * $3 )!g;
         s!KStringHandler::cEmSqueeze\s*\(\s*(.+)\s*,\s*(.+)\s*,\s*(.+)\s*\)!$2.elidedText( $1, Qt::ElideMiddle, $2.maxWidth() * $3 )!g;
         s!KStringHandler::rEmSqueeze\s*\(\s*(.+)\s*,\s*(.+)\s*,\s*(.+)\s*\)!$2.elidedText( $1, Qt::ElideRight, $2.maxWidth() * $3 )!g;
+
+        s!KStringHandler::lPixelSqueeze\s*\(\s*(.+)\s*,\s*(.+)\s*,\s*(.+)\s*\)!$2.elidedText( $1, Qt::ElideLeft, $3 )!g;
+        s!KStringHandler::cPixelSqueeze\s*\(\s*(.+)\s*,\s*(.+)\s*,\s*(.+)\s*\)!$2.elidedText( $1, Qt::ElideMiddle, $3 )!g;
+        s!KStringHandler::rPixelSqueeze\s*\(\s*(.+)\s*,\s*(.+)\s*,\s*(.+)\s*\)!$2.elidedText( $1, Qt::ElideRight, $3 )!g;
+
     } $file;
     functionUtilkde::removeIncludeInFile( $file, "kstringhandler.h");
 }
