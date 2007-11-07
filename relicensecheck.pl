b8d680b0bbe28bd5b1228f6aa105ba1b6bb68be5
Dirk Mueller <mueller@kde.org>
2007-11-07 22:57:03 +0000

    adding a script to check for relicensing possibility. Usage: $0 <file_or_dir>, e.g.
    relicensecheck.pl kdebase/khotkeys
    
    which gives among a detailed list:
    
    Summary:
       69 commits possibly violating lgplv23
       69 commits possibly violating gplv23
       70 commits possibly violating gplv2+
       70 commits possibly violating lgplv2+
    
    
    svn path=/trunk/KDE/kdesdk/scripts/; revision=734055

---
+++ relicensecheck.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/perl -w
+# vim:sw=4:et
+# (c) Dirk Mueller. GPLv2+
+# I would love to be a python script, but os.popen just sucks
+
+use strict;
+
+my %whitelist;
+my %blacklist;
+my @blacklist_revs;
+
+foreach my $who(
+    'cschlaeg',
+    'dfaure',
+    'espen',
+    'granroth',
+    'johnflux',
+    'martyn',
+    'mueller',
+    'rich',
+    'trueg',
+    'waba',
+) {
+    $whitelist{"gplv23"}->{$who} = 1;
+}
+
+foreach my $who(
+    'cschlaeg',
+    'dfaure',
+    'espen',
+    'granroth',
+    'johnflux',
+    'martyn',
+    'mueller',
+    'rich',
+    'trueg',
+    'waba',
+) {
+    $whitelist{"lgplv23"}->{$who} = 1;
+}
+
+foreach my $who(
+    'dfaure',
+    'espen',
+    'granroth',
+    'johnflux',
+    'martyn',
+    'mueller',
+    'trueg',
+) {
+    $whitelist{"gplv2+"}->{$who} = 1;
+}
+
+foreach my $who(
+    'dfaure',
+    'espen',
+    'granroth',
+    'johnflux',
+    'martyn',
+    'mueller',
+    'trueg',
+) {
+    $whitelist{"lgplv2+"}->{$who} = 1;
+}
+
+my $file = $ARGV[0] || "";
+
+die "need existing file: $file" if (! -r $file);
+
+open(IN, "-|") || exec 'svn', 'log', '-q', $file;
+while(<IN>) {
+
+    if (/^r(\d+) \| (\S+) /)  {
+        my ($rev, $author) = ($1, $2);
+
+        next if ($author eq "scripty");
+
+        foreach my $license(keys %whitelist) {
+            if (!defined($whitelist{$license}->{$author})) {
+                push(@{$blacklist{$license}->{$author}}, $rev);
+            }
+        }
+    }
+}
+close(IN);
+
+if (defined (keys %blacklist)) {
+    print "Need permission for licensing:\n\n";
+
+    my %stat;
+
+    foreach my $license(keys %blacklist) {
+        print "- $license:\n";
+        foreach my $who(keys %{$blacklist{$license}}) {
+            $stat{$license} += length(@{$blacklist{$license}->{$who}});
+            printf "%8s:  %s\n", $who, join(",", @{$blacklist{$license}->{$who}});
+        }
+        print "\n";
+    }
+
+    print "\n";
+    print "Summary:\n";
+
+    foreach my $license(sort { $stat{$a} <=> $stat{$b} } keys %stat) {
+        printf "%5d commits possibly violating %s\n", $stat{$license}, $license
+    }
+}
