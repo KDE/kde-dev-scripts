1b1fda0cec6345d8b6574acd3cc355ee1dc1923f
Laurent Montel <montel@kde.org>
2007-11-12 11:59:24 +0000

    QToolTip convertion
    svn path=/trunk/KDE/kdesdk/scripts/; revision=735675

--- qt4/convert-whatsthis.pl
+++ qt4/convert-whatsthis.pl
@@ -3,6 +3,9 @@
 s!Q(3|)WhatsThis::add\s*\(\s*([^,]*),!\2->setWhatsThis(!g;
 s!#include <q3whatsthis.h>!!g;
 
+s!QToolTip::add\s*\(\s*([^,]*),!\1->setToolTip(!;
+
+
 # Q(V|H)GroupBox convertion
 # TODO add #include <Q3GroupBox>
 s!new\s*QVGroupBox\s*\(!new Q3GroupBox\(1, Qt::Horizontal,!g;
