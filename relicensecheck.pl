2c7af026eae24cfe02456b37e975505fead271b0
Tom Albers <toma@kde.org>
2008-02-08 22:42:23 +0000

    Received a mail from Toshitaka: ------------------ 1.Are you okay with relicensing your contributions done under GPLv2 to GPLv2+?   yes
     2.Are you okay with relicensing your contributions done under GPLv2 to GPLv2 or GPLv3 ? 
      yes
     
     3.Are you okay with relicensing your contributions done under LGPLv2 to LGPLv2+? 
      yes
     
     4.Are you okay with relicensing your contributions done under LGPLv2 to LGPLv2 or LGPLv3? 
      yes
     
     5.Are you okay with the KDE e.V. deciding on a future licensing change to your code, should that be necessary? 
      yes
     
     Regards,
     Toshitaka
    
    svn path=/trunk/KDE/kdesdk/scripts/; revision=772504

--- relicensecheck.pl
+++ relicensecheck.pl
@@ -28,6 +28,7 @@ my %license_table = (
     'ereslibre' => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'eros'      => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'fawcett'   => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
+    'fujioka'   => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'haeckel'   => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+',       ],
     'hoelzer'   => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'jlee'      => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
