da68a64f5302e0313c16d8b0f4c2d0607f5291fb
Erlend Hamberg <ehamberg@gmail.com>
2010-02-16 14:00:14 +0000

    added myself (ehamberg) to the list
    svn path=/trunk/KDE/kdesdk/scripts/; revision=1090990

--- relicensecheck.pl
+++ relicensecheck.pl
@@ -48,6 +48,7 @@ my %license_table = (
     'dfaure'    => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'dhaumann'  => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'dyp'       => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
+    'ehamberg'  => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'ereslibre' => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'eros'      => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'eschepers' => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+',       ],
