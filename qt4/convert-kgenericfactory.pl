9eebc48d9ed4796a71240b95bc27d45c18a29f71
David Faure <faure@kde.org>
2010-11-03 21:27:46 +0000

    Script for converting KGenericFactory + K_EXPORT_COMPONENT_FACTORY to K_PLUGIN_FACTORY + K_EXPORT_PLUGIN. This requires changing the plugin's ctor from T*, QStringList to QObject*, QVariantList, though.
    svn path=/trunk/KDE/kdesdk/scripts/; revision=1192728

---
+++ qt4/convert-kgenericfactory.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl -w
+
+# David Faure <faure@kde.org>
+# KGenericFactory + K_EXPORT_COMPONENT_FACTORY -> K_PLUGIN_FACTORY + K_EXPORT_PLUGIN
+
+use strict;
+use File::Basename;
+use lib dirname($0);
+use functionUtilkde;
+
+my $plugin;
+my $factory;
+
+foreach my $file (@ARGV) {
+    functionUtilkde::substInFile {
+        s!kgenericfactory\.h!kpluginfactory.h!g;
+        if (/typedef KGenericFactory<(.*)>\s*(.*);/) {
+            $plugin = $1;
+            $factory = $2;
+            # Ignore ",BaseClassForParentArg" in $plugin
+            if ($plugin =~ s/,(.*)//) {
+                print STDERR "Warning, parent argument for plugin was $1, needs to be changed to QObject, or you need a create function\n";
+                print STDERR "  (see koffice/libs/main/KoDocInfoPropsFactory.cpp for an example)\n";
+            }
+            $_ = "K_PLUGIN_FACTORY($factory, registerPlugin<$plugin>();)\n";
+        }
+        if (/K_EXPORT_COMPONENT_FACTORY\(\s*(\w*),\s*(\w*)\(\s*(\"\w*\")\s*\)\s*\)/) {
+            my $libname_ignored = $1;
+            my $factory2 = $2;
+            my $catalogname = $3;
+            die "Expected $factory, got $factory2" if ($factory ne $factory2);
+            $_ = "K_EXPORT_PLUGIN($factory($catalogname))\n";
+        }
+    } $file;
+}
+functionUtilkde::diffFile( "@ARGV" );
