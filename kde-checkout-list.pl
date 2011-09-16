fbab5b380c0fac8c105c95abd3f5a56c2849fe6b
Jeremy Paul Whiting <jpwhiting@kde.org>
2011-09-16 01:41:42 +0000

    Kdeplasma-addons branches conform to KDE/X.Y now.
    svn path=/trunk/KDE/kdesdk/scripts/; revision=1253877

--- kde-checkout-list.pl
+++ kde-checkout-list.pl
@@ -131,14 +131,14 @@ foreach $proj (sort keys %output) {
       my $command;
       if ( ! -d "$subdir" ) {
 #modules without the "KDE/" in the branchname are:
-#  kdepim*, kdeplasma-addons, kdeutils
+#  kdepim*, kdeutils
 # kdebase/kate => only KDE/4.7 and above
 # kdeexamples => No branches
 # superbuild => No branches
 
 	if ( $branch ) {
 	  next if ( $subdir =~ m+/kdeexamples+ || $subdir =~ m+/superbuild+ );
-	  if ( $subdir =~ m+/kde(pim|utils|plasma-addons)+ ||
+	  if ( $subdir =~ m+/kde(pim|utils)+ ||
 	       $subdir =~ m+/okular+ ||
 	       $subdir =~ m+/mobipocket+ ) {
 	    $command = "git clone $url $subdir && cd $subdir && git checkout -b $branch origin/$branch";
@@ -151,7 +151,7 @@ foreach $proj (sort keys %output) {
       } else {
 	if ($branch) {
 	  next if ( $subdir =~ m+/kdeexamples+ || $subdir =~ m+/superbuild+ );
-	  if ( $subdir =~ m+/kde(pim|utils|plasma-addons)+ ||
+	  if ( $subdir =~ m+/kde(pim|utils)+ ||
 	       $subdir =~ m+/okular+ ||
 	       $subdir =~ m+/mobipocket+ ) {
 	    $command = "cd $subdir && git config remote.origin.url $url && git checkout $branch && git pull --ff";
