ab5ee6e4635815146669030852682f0edd8fc93f
David Faure <faure@kde.org>
2012-06-04 23:01:45 +0000

    Fully automated porting of findExe (as much as possible) Port other stuff along the way. Fix includes automatically.
    svn path=/trunk/KDE/kdesdk/scripts/; revision=1298355

--- kf5/convert-kstandarddirs.pl
+++ kf5/convert-kstandarddirs.pl
@@ -30,27 +30,33 @@ my %xdgconfResource = (
 );
 
 foreach my $file (@ARGV) {
+    my $addconfigprefix = 0;
+
     functionUtilkde::substInFile {
-	s/KGlobal::dirs\(\)->locate/KStandardDirs::locate/;
+        s/KGlobal::dirs\(\)->findResource/KStandardDirs::locate/;
+        s/KGlobal::dirs\(\)->locate/KStandardDirs::locate/;
+        s/KGlobal::dirs\(\)->findExe/KStandardDirs::findExe/;
+        s/KStandardDirs::locate\("exe", /KStandardDirs::findExe\(/;
+        s/KGlobal::dirs\(\)->saveLocation/KStandardDirs::saveLocation/;
         if (/KStandardDirs::locateLocal\(\s*\"(.*)\",\s*(.*)\s*\)/) {
             my $resource = $1;
             my $fileName = $2;
-	    my $loc;
+            my $loc;
             if (defined $easyResource{$resource}) {
                 $loc = $easyResource{$resource};
-		$fileName = "QLatin1Char('\/') + $fileName";
+                $fileName = "QLatin1Char('\/') + $fileName";
             } elsif (defined $otherResource{$resource}) {
-		$loc = "QStandardPaths::GenericDataLocation";
-		$fileName = "QLatin1String(\"$otherResource{$resource}\/\") + $fileName";
+                $loc = "QStandardPaths::GenericDataLocation";
+                $fileName = "QLatin1String(\"$otherResource{$resource}\/\") + $fileName";
             } elsif (defined $xdgconfResource{$resource}) {
-		$loc = "QStandardPaths::ConfigLocation";
-		$fileName = "QLatin1String(\"$xdgconfResource{$resource}\/\") + $fileName";
+                $loc = "QStandardPaths::ConfigLocation";
+                $fileName = "QLatin1String(\"$xdgconfResource{$resource}\/\") + $fileName";
             } else {
-		print STDERR "Unhandled resource $resource\n";
-	    }
-	    if (defined $loc) {
-		s/KStandardDirs::locateLocal\(.*\)/QStandardPaths::writableLocation($loc) + $fileName/;
-	    }
+                print STDERR "Unhandled resource $resource\n";
+            }
+            if (defined $loc) {
+                s/KStandardDirs::locateLocal\(.*\)/QStandardPaths::writableLocation($loc) + $fileName/;
+            }
         }
         if (/KStandardDirs::locate\(\s*\"(.*)\",\s*(.*)\s*\)/) {
             my $resource = $1;
@@ -59,24 +65,63 @@ foreach my $file (@ARGV) {
             if (defined $easyResource{$resource}) {
                 $loc = $easyResource{$resource};
             } elsif (defined $otherResource{$resource}) {
-		$loc = "QStandardPaths::GenericDataLocation";
-		$fileName = "QLatin1String(\"$otherResource{$resource}\/\") + $fileName";
+                $loc = "QStandardPaths::GenericDataLocation";
+                $fileName = "QLatin1String(\"$otherResource{$resource}\/\") + $fileName";
             } elsif (defined $xdgconfResource{$resource}) {
-		$loc = "QStandardPaths::ConfigLocation";
-		$fileName = "QLatin1String(\"$xdgconfResource{$resource}\/\") + $fileName";
+                $loc = "QStandardPaths::ConfigLocation";
+                $fileName = "QLatin1String(\"$xdgconfResource{$resource}\/\") + $fileName";
             } else {
-		print STDERR "Unhandled resource $resource\n";
-	    }
-	    if (defined $loc) {
-		# ends with a '/' (in a string literal) ?
-		print STDERR "fileName=$fileName\n";
-		if ($fileName =~ s/\/\"$/\"/ || $fileName =~ s/\/\"\)$/\"\)/) {
-		    s/KStandardDirs::locate\(.*\)/QStandardPaths::locate($loc, $fileName, QStandardPaths::LocateDirectory)/;
-		} else {
-		    s/KStandardDirs::locate\(.*\)/QStandardPaths::locate($loc, $fileName)/;
-		}
-	    }
+                print STDERR "Unhandled resource $resource\n";
+            }
+            if (defined $loc) {
+                # ends with a '/' (in a string literal) ?
+                print STDERR "fileName=$fileName\n";
+                if ($fileName =~ s/\/\"$/\"/ || $fileName =~ s/\/\"\)$/\"\)/) {
+                    s/KStandardDirs::locate\(.*\)/QStandardPaths::locate($loc, $fileName, QStandardPaths::LocateDirectory)/;
+                } else {
+                    s/KStandardDirs::locate\(.*\)/QStandardPaths::locate($loc, $fileName)/;
+                }
+            }
+        }
+        if (/KStandardDirs::findExe\(\s*\"(.*)\"(,\s*[^\)]*)?\s*\)/ ||
+            /KStandardDirs::findExe\(\s*QLatin1String\s*\(\"(.*)\"\)(,\s*[^\)]*)?\s*\)/) {
+            my $exe = $1;
+            if (`which $exe 2>/dev/null`) {
+                print STDERR "found $exe\n";
+                s/KStandardDirs::findExe/QStandardPaths::findExecutable/;
+            } else {
+                if (`locate libexec/$exe`) {
+                    s/KStandardDirs::findExe\([^\)]*\)/CMAKE_INSTALL_PREFIX \"\/\" LIBEXEC_INSTALL_DIR \"\/$exe\"/;
+                    #print STDERR "$exe NOT found in PATH, use CMAKE_INSTALL_PREFIX \"/\" LIBEXEC_INSTALL_DIR \"/$exe\" instead\n";
+                    $addconfigprefix = 1;
+                } else {
+                    print STDERR "$exe NOT found in PATH, nor in libexec, using locate. Where is it?\n";
+                }
+            }
+        }
+        if (/KStandardDirs::saveLocation\(\s*\"(.*)\"\s*\)/) {
+            my $resource = $1;
+            my $loc;
+            my $add = "";
+            if (defined $easyResource{$resource}) {
+                $loc = $easyResource{$resource};
+            } elsif (defined $otherResource{$resource}) {
+                $loc = "QStandardPaths::GenericDataLocation";
+                $add = " + QLatin1String(\"$otherResource{$resource}\/\")";
+            } elsif (defined $xdgconfResource{$resource}) {
+                $loc = "QStandardPaths::ConfigLocation";
+                $add = " + QLatin1String(\"$otherResource{$resource}\/\")";
+            }
+            s/KStandardDirs::saveLocation\(\s*\"$resource\"\s*\)/QStandardPaths::writableLocation($loc)$add/;
         }
     } $file;
+    functionUtilkde::addIncludeInFile($file, "config-prefix.h") if ($addconfigprefix);
+    if (not `grep -i dirs $file | grep -v kstandarddirs\.h`) {
+      functionUtilkde::removeIncludeInFile($file, "kstandarddirs.h");
+    }
+    if (`grep QStandardPaths $file | grep -v '#include'`) {
+      functionUtilkde::addIncludeInFile($file, "qstandardpaths.h");
+    }
 }
+
 functionUtilkde::diffFile( "@ARGV" );
