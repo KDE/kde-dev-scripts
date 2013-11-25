5f1fd8f1ba42c9e09ec20bc2b591b8c26ba73707
David Faure <faure@kde.org>
2013-11-25 10:37:40 +0100

    Script to split out kdelibs into N frameworks, setting things up for git graft.

---
+++ frameworks/split_out_frameworks.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# This script splits out all the frameworks from kdelibs into separate repositories
+# and puts them into ../frameworks/
+
+origproject=kdelibs
+origbranch=frameworks
+origsha1=`git rev-parse HEAD`
+
+if [ ! -d tier1 ]; then
+  "echo Run this script from the toplevel of the monolithic repository, there must be a tier1 subdirectory"
+  exit 1
+fi
+
+dest=../frameworks
+mkdir -p $dest
+here=$PWD
+
+ls -d tier1/* tier2/* tier3/* tier4/* | while read dir; do
+    cd $here
+    frameworkname=`basename $dir`
+
+    if [ $frameworkname = 'CMakeLists.txt' ]; then
+      continue;
+    fi
+
+    rm -rf $dest/$frameworkname
+    cp -a $dir $dest/
+    cd $dest/$frameworkname
+
+git init
+git add .
+
+git commit -F - <<EOF
+
+Initial import from the monolithic $origproject.
+
+This is the beginning of revision history for this module. If you
+want to look at revision history older than this, please refer to the
+techbase wiki for how to use Git history grafting. At the time of
+writing, this wiki is located here:
+
+http://community.kde.org/Frameworks/GitOldHistory
+
+If you have already performed the grafting and you don't see any
+history beyond this commit, try running "git log" with the "--follow"
+argument.
+
+Branched from the monolithic repo, $origproject $origbranch branch, at commit
+$origsha1
+
+EOF
+
+done
+
