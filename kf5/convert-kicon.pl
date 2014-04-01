11781a773b3875f1563764f69126ecabdb8f7a1e
Montel Laurent <montel@kde.org>
2014-04-01 19:45:26 +0200

    Script to convert KIcon to QIcon::fromTheme

---
+++ kf5/convert-kicon.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+
+# Laurent Montel <montel@kde.org> (2014)
+# KIcon("...") => QIcon::fromTheme("...")
+# find -iname "*.cpp"|xargs kde-dev-scripts/kf5/convert-kicon.pl
+
+use strict;
+use File::Basename;
+use lib dirname($0);
+use functionUtilkde;
+
+foreach my $file (@ARGV) {
+
+    my $modified;
+    open(my $FILE, "<", $file) or warn "We can't open file $file:$!\n";
+    my @l = map {
+        my $orig = $_;
+        s/\bKIcon\b\s*\(/QIcon::fromTheme(/;
+        s/\<KIcon\b\>/\<QIcon>/ =~ /#include/ ;
+
+        $modified ||= $orig ne $_;
+        $_;
+    } <$FILE>;
+
+    if ($modified) {
+        open (my $OUT, ">", $file);
+        print $OUT @l;
+        close ($OUT);
+    }
+}
+
+functionUtilkde::diffFile( "@ARGV" );
