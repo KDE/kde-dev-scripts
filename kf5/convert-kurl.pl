46d5df299031fc192fac0a7557bb068c4abed300
David Faure <faure@kde.org>
2014-04-21 20:33:40 +0200

    KUrl -> QUrl porting

--- kf5/convert-kurl.pl
+++ kf5/convert-kurl.pl
@@ -2,18 +2,15 @@
 
 # David Faure <faure@kde.org>
 # KUrl -> QUrl
-# QUrlPathInfo -> QUrl
-#    (QUrlPathInfo was a temporary API in kdelibs-frameworks, before QUrl was extended in Qt 5.2)
+# Usage: convert-kurl.pl *.h *.cpp
 
 use strict;
 use File::Basename;
 use lib dirname($0);
 use functionUtilkde;
+my %urls = (); # shared between all files
 
 foreach my $file (@ARGV) {
-
-    my $infoVar;
-    my $urlVar;
 
     # I don't use functionUtilkde::substInFile because it touches all files, even those which were not modified.
     my $modified;
@@ -21,51 +18,90 @@ foreach my $file (@ARGV) {
     my @l = map {
         my $orig = $_;
 
-        # kdelibs: QUrlPathInfo pathInfo(url);
-        if (/QUrlPathInfo (\w*)\(\s*([\.\-\>_\w]*)\s*\)/) {
-            # Record info->url association
-            $infoVar = $1;
-            $urlVar = $2;
-            #print STDERR "infoVar=$infoVar urlVar=$urlVar\n";
-        } elsif (/^}/) {  # end of method
-            undef $infoVar;
+        s/#include <kurl.h>/#include <QUrl>/;
+        s/#include <KUrl>/#include <QUrl>/;
+        s/class KUrl\;/class QUrl\;/;
+        s/KUrl\(\)\;/QUrl()\;/;
+
+        # Detect variables being declared as KUrl
+        if (/const KUrl\s*&\s*(\w+)/ || /^\s*KUrl\s+(\w+)\s*[=\;]/ || /^\s*KUrl\s+(\w+)\s*\(/) {
+            $urls{$1} = 1;
+            print STDERR "found KUrl var: $1\n";
+            s/const KUrl\s*&\s*/const QUrl &/;
+            s/KUrl (\w+)\;/QUrl $1\;/;
+            s/KUrl (\w+)\s*=/QUrl $1 =/;
         }
-        # kdelibs: pathInfo.adjustPath(QUrlPathInfo::StripTrailingSlash);
-        #    => url = url.adjusted(QUrl::StripTrailingSlash);
-        if (defined $infoVar && /$infoVar\.adjustPath\(\s*QUrlPathInfo::StripTrailingSlash\s*\)/) {
-            s/$infoVar.adjustPath\(\s*QUrlPathInfo::StripTrailingSlash\s*\)/$urlVar = $urlVar\.adjusted(QUrl::StripTrailingSlash)/;
+        # Detect variables being declared as QUrl (for copying into KUrls, below)
+        if (/const QUrl\s*&\s*(\w+)/ || /^\s*QUrl\s+(\w+)\s*(?:=|;)/ || /^\s*QUrl\s+(\w+)\s*\(/) {
+            #print STDERR "found QUrl var: $1\n";
+            $urls{$1} = 2;
         }
-        # kdelibs: QUrlPathInfo::adjustPath(url, QUrlPathInfo::StripTrailingSlash);
-        #    => url = url.adjusted(QUrl::StripTrailingSlash);
-        s/QUrlPathInfo::adjustPath\(\s*(\w*)\s*,\s*QUrlPathInfo::StripTrailingSlash\s*\)/$1 = $1\.adjusted(QUrl::StripTrailingSlash)/;
-        # KDE4 code: url.adjustPath(KUrl::RemoveTrailingSlash);
-        #     => url = url.adjusted(QUrl::StripTrailingSlash);
+
+        # Do not port KUrl(QString) automatically, that's impossible (need to find out if the string is a path or a URL...)
+        # Except when it's clear :)
+        if (my ($var, $value) = /KUrl (\w+)\s*\(\s*(\"[^\"]*\")\s*\)/) {
+            if ($value =~ /:\//) { # full URL -> clear
+                s/KUrl $var/QUrl $var/;
+            }
+        }
+        if (my ($value) = /KUrl\s*\(\s*(\"[^\"]*\")\s*\)/) { # anonymous var
+            if ($value =~ /:\//) { # full URL -> clear
+                s/KUrl\s*\(\s*\Q$value\E\s*\)/QUrl($value)/;
+            }
+        }
+        if (my ($var, $value) = /KUrl (\w+)\s*\(\s*(\w+)\s*\)/) {
+            if (defined $urls{$value}) { # copy of another URL -> clear
+                s/KUrl $var/QUrl $var/;
+            }
+        }
+
+        if (/(\w+)\.protocol\(\)/ && defined $urls{$1}) {
+            my $url = $1;
+            s/$url\.protocol\(\)/$url\.scheme()/g;
+        }
+        if (/(\w+)\.setProtocol\(/ && defined $urls{$1}) {
+            my $url = $1;
+            s/$url\.setProtocol\(/$url\.setScheme(/g;
+        }
+        if (/(\w+)\.user\(\)/ && defined $urls{$1}) {
+            my $url = $1;
+            s/$url\.user\(\)/$url\.userName()/g;
+        }
+        if (/(\w+)\.hasRef\(\)/ && defined $urls{$1}) {
+            my $url = $1;
+            s/$url\.hasRef\(\)/$url\.hasFragment()/g;
+        }
+
+        # url.adjustPath(KUrl::RemoveTrailingSlash) => url = url.adjusted(QUrl::StripTrailingSlash);
         if (/(\w*).adjustPath\(\s*KUrl::RemoveTrailingSlash\s*\)/) {
             my $urlvar = $1;
             s/adjustPath\(\s*KUrl::RemoveTrailingSlash\s*/$urlvar = $urlvar\.adjusted(QUrl::StripTrailingSlash)/;
         }
-
-        # kdelibs: QUrlPathInfo(url1).url(QUrlPathInfo::StripTrailingSlash);
-        s/QUrlPathInfo\(\s*([\.\-\>_\w]*)\s*\)\.url\(QUrlPathInfo::StripTrailingSlash\)/$1\.adjusted(QUrl::StripTrailingSlash)/g;
-
-        # kdelibs: QUrlPathInfo(url).directory()
-        s/QUrlPathInfo\(\s*([\.\-\>_\w]*)\s*\)\.directory\(\)/$1\.adjusted(QUrl::RemoveFilename|QUrl::StripTrailingSlash).path()/g;
-        # kdelibs: QUrlPathInfo(url).fileName()
-        s/QUrlPathInfo\(\s*([\.\-\>_\w]*)\s*\)\.fileName\(\)/$1\.fileName()/g;
-        # kdelibs: QUrlPathInfo(url).directoryUrl() -> url.adjusted(QUrl::RemoveFilename|QUrl::StripTrailingSlash)
-        s/QUrlPathInfo\(\s*([\.\-\>_\w]*)\s*\)\.directoryUrl\(\)/$1\.adjusted(QUrl::RemoveFilename|QUrl::StripTrailingSlash)/g;
-
-        # pathInfo.fileName()
-        if (defined $infoVar && /$infoVar\.fileName\(\)/) {
-            s/$infoVar\.fileName\(\)/$urlVar\.fileName()/g;
+        # url.directory()
+        if (/(\w+)\.directory\(\)/ && defined $urls{$1}) {
+            my $url = $1;
+            s/$url\.directory\(\)/$url\.adjusted(QUrl::RemoveFilename|QUrl::StripTrailingSlash).path()/g;
         }
-        # pathInfo.directory()
-        if (defined $infoVar && /$infoVar\.directory\(\)/) {
-            s/$infoVar\.directory\(\)/$urlVar\.adjusted(QUrl::RemoveFilename|QUrl::StripTrailingSlash).path()/g;
+        # url.path(KUrl::RemoveTrailingSlash)
+        if (/(\w+)\.path\(KUrl::RemoveTrailingSlash\)/ && defined $urls{$1}) {
+            my $url = $1;
+            s/$url\.path\(KUrl::RemoveTrailingSlash\)/$url\.adjusted(QUrl::StripTrailingSlash).path()/g;
         }
-        # pathInfo.localPath(QUrlPathInfo::StripTrailingSlash)
-        if (defined $infoVar && /$infoVar\.localPath\(QUrlPathInfo::StripTrailingSlash\)/) {
-            s/$infoVar\.localPath\(QUrlPathInfo::StripTrailingSlash\)/$urlVar\.adjusted(QUrl::StripTrailingSlash).toLocalFile()/g;
+        # url.toLocalFile(KUrl::RemoveTrailingSlash)
+        if (/(\w+)\.toLocalFile\(KUrl::RemoveTrailingSlash\)/ && defined $urls{$1}) {
+            my $url = $1;
+            s/$url\.toLocalFile\(KUrl::RemoveTrailingSlash\)/$url\.adjusted(QUrl::StripTrailingSlash).toLocalFile()/g;
+        }
+        # url.adjustPath(KUrl::RemoveTrailingSlash)
+        if (/(\w+)\.adjustPath\(KUrl::RemoveTrailingSlash\)/ && defined $urls{$1}) {
+            my $url = $1;
+            s/$url\.adjustPath\(KUrl::RemoveTrailingSlash\)/$url = $url\.adjusted(QUrl::StripTrailingSlash)/g;
+        }
+        # url.addPath(path)
+        if (my ($indent, $url, $path) = /^(\s*)(\w+)\.addPath\(\s*([^\)]*)\s*\)/) {
+            if (defined $urls{$url}) {
+                s/$url\.addPath\(\s*$path\s*\)\;/$url = $url\.adjusted(QUrl::StripTrailingSlash)\;\n$indent$url.setPath($url.path() + '\/' + $path)\;/;
+            }
         }
 
         $modified ||= $orig ne $_;
