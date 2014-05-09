d386df5d803272dd3acf7e11b60b7e551e38bba5
Montel Laurent <montel@kde.org>
2014-05-09 20:16:01 +0200

    Convert kglobal::locale

---
+++ kf5/convert-kglobal-locale.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+
+# Laurent Montel <montel@kde.org> (2014)
+# KGlobal::locale() -> KLocale::global()
+# find -iname "*.cpp"|xargs kde-dev-scripts/kf5/convert-kglobal-locale.pl
+
+use strict;
+use File::Basename;
+use lib dirname($0);
+use functionUtilkde;
+
+foreach my $file (@ARGV) {
+
+    my $modified;
+    open(my $FILE, "<", $file) or warn "We can't open file $file:$!\n";
+    my @l = map {
+        my $orig = $_;
+        s/\bKGlobal::locale\s*\(\)/KLocale::global\(\)/;
+
+        $modified ||= $orig ne $_;
+        $_;
+    } <$FILE>;
+
+    if ($modified) {
+        open (my $OUT, ">", $file);
+        print $OUT @l;
+        close ($OUT);
+        functionUtilkde::addIncludeInFile($file, "KLocale");
+    }
+}
+
+functionUtilkde::diffFile( "@ARGV" );
