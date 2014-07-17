06760df6522e0ff27b2bcea3a83fe0a51538e159
Montel Laurent <montel@kde.org>
2014-07-17 09:39:34 +0200

    Add script to convert KTextBrowser to QTextBrowser

---
+++ kf5/convert-ktextbrowser.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+
+# Laurent Montel <montel@kde.org> (2014)
+# KTextBrowser => QTextBrowser
+# find -iname "*.cpp" -o -iname "*.h" -o -iname "*.ui" |xargs kde-dev-scripts/kf5/convert-ktextbrowser.pl
+
+use strict;
+use File::Basename;
+use lib dirname($0);
+use functionUtilkde;
+
+foreach my $file (@ARGV) {
+
+    my $modified;
+    open(my $FILE, "<", $file) or warn "We can't open file $file:$!\n";
+    my @l = map {
+        my $orig = $_;
+        s/\bKTextBrowser\b/QTextBrowser/g;
+        s/\<KTextBrowser\b\>/\<QTextBrowser>/ if (/#include/);
+        s/\<ktextbrowser.h\>/\<QTextBrowser>/ if (/#include/);
+        
+        $modified ||= $orig ne $_;
+        $_;
+    } <$FILE>;
+
+    if ($modified) {
+        open (my $OUT, ">", $file);
+        print $OUT @l;
+        close ($OUT);
+        warn "QTextBrowser does not support \"whatsthis:\" urls. \n";
+    }
+}
+
+functionUtilkde::diffFile( "@ARGV" );
