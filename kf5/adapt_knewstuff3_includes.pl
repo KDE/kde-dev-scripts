8ba4009fe7974a529982039924cb3b78f536dd18
Montel Laurent <montel@kde.org>
2014-07-17 08:11:47 +0200

    Fix script name

--- kf5/adapt_knewstuff3_includes.pl
+++ kf5/adapt_knewstuff3_includes.pl
@@ -2,7 +2,7 @@
 
 # Laurent Montel <montel@kde.org> (2014)
 # Adapt new akonadi includes
-# find -iname "*.cpp"|xargs kde-dev-scripts/kf5/adapt_knewstuff3_includes.pl.pl
+# find -iname "*.cpp"|xargs kde-dev-scripts/kf5/adapt_knewstuff3_includes.pl
 
 use strict;
 use File::Basename;
