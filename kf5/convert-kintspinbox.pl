b440a3559a15d5c0914ef473ed11acf659784dcc
Montel Laurent <montel@kde.org>
2014-07-24 09:20:50 +0200

    convert setRange

--- kf5/convert-kintspinbox.pl
+++ kf5/convert-kintspinbox.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 
 # Laurent Montel <montel@kde.org> (2014)
-# KIntSpinbox -> QIntSpinBox
+# KIntSpinbox -> QSpinBox
 # find -iname "*.cpp"|xargs kde-dev-scripts/kf5/convert-kintspinbox.pl
 
 use strict;
@@ -10,6 +10,7 @@ use lib dirname($0);
 use functionUtilkde;
 
 foreach my $file (@ARGV) {
+    my %varname = ();
 
     my $modified;
     open(my $FILE, "<", $file) or warn "We can't open file $file:$!\n";
@@ -27,6 +28,7 @@ foreach my $file (@ARGV) {
           warn "KIntSpinBox found $var $argument\n";
           #KIntSpinBox(int lower, int upper, int singleStep, int value, QWidget *parent, int base = 10);
 
+          $varname{$var} = 1;
 
           my ($lower, $upper, $singleStep, $value, $parent, $base, $after);
           my $constructor_regexp = qr/
@@ -50,8 +52,32 @@ foreach my $file (@ARGV) {
              }
           }
         }
-        s!#include \<KIntSpinBox\>!#include \<QSpinBox\>!;
-        s!class KIntSpinBox;!class QSpinBox;!;
+       my $regexpRange = qr/
+           ^(\s*)                        # (1) Indentation, possibly "Classname *" (the ? means non-greedy)
+           (\w+)                         # (2) variable name
+           \->setRange\s*\((.*)\)        # (3) argument
+           (.*)$                         # (4) afterreg
+           /x; # /x Enables extended whitespace mode
+        if (my ($indent, $var, $argument, $afterreg) = $_ =~ $regexpRange) {
+            warn "found setRange \'$var\'\n";
+            if ( defined $varname{$var} ) {
+                 my $argument_regexp = qr/
+                                 ^([^,]*)\s*        # mini
+                                 ,\s*([^,]*)\s*     # maxi
+                                 ,\s([^,]*)         # singleStep
+                                 (.*)$              # after
+                                 /x;
+                 if (my ($minimum, $maximum, $singleStep) = $argument =~ $argument_regexp) {
+                    $_ = $indent . "$var->setRange($minimum, $maximum);\n";
+                    $_ .=  $indent . "$var->setSingleStep($singleStep);\n";
+                 }
+            }
+        }
+
+        s/\bKIntSpinBox\b/QSpinBox/g;
+        s/\<KIntSpinBox\b\>/\<QSpinBox>/ if (/#include/);
+        s/\<kintspinbox.h\>/\<QSpinBox>/ if (/#include/);
+
         s!KIntSpinBox!QSpinBox!;
         $modified ||= $orig ne $_;
         $_;
