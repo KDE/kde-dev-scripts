a11d4b8417fffbad773b8411e1684e1c974d50c8
Montel Laurent <montel@kde.org>
2014-07-25 07:54:20 +0200

    Port qt macro Q_WS_* -> Q_OS_*

---
+++ kf5/convert-qt-os-macro.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+
+# Laurent Montel <montel@kde.org> (2014)
+# Q_WS* -> Q_OS*
+# find -iname "*.cpp"|xargs kde-dev-scripts/kf5/convert-qt-os-macro.pl
+
+use strict;
+use File::Basename;
+use lib dirname($0);
+use functionUtilkde;
+
+foreach my $file (@ARGV) {
+
+    my $modified;
+    open(my $FILE, "<", $file) or warn "We can't open file $file:$!\n";
+    my @l = map {
+        my $orig = $_;
+        s/\bQ_WS_/Q_OS_/g;
+        $modified ||= $orig ne $_;
+        $_;
+    } <$FILE>;
+
+    if ($modified) {
+        open (my $OUT, ">", $file);
+        print $OUT @l;
+        close ($OUT);
+    }
+}
+
+functionUtilkde::diffFile( "@ARGV" );
