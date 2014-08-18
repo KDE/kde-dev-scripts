33d99010df230fcf32c92fb78f6423762f43c8cb
Montel Laurent <montel@kde.org>
2014-08-18 08:37:07 +0200

    Start to convert getSaveFileName

--- kf5/convert-kfiledialog.pl
+++ kf5/convert-kfiledialog.pl
@@ -74,7 +74,7 @@ foreach my $file (@ARGV) {
            (.*?)                         # (2) Possibly "Classname *" (the ? means non-greedy)
            (\w+)                         # (3) variable name
            \s*=\s*                       #   assignment
-           KFileDialog::getExistingDirectory\s*\((.*)\)  # (4)  new KPushButton(...,...,...,...);
+           KFileDialog::getExistingDirectory\s*\((.*)\)  # (4)  KFileDialog::getExistingDirectory(...)
            (.*)$                         # (5) afterreg
            /x; # /x Enables extended whitespace mode
         if (my ($indent, $left, $var, $argument, $afterreg) = $_ =~ $regexgetExistingDirectory) {
@@ -110,6 +110,69 @@ foreach my $file (@ARGV) {
               $needQFileDialog = 1;
            }
         }
+
+        my $regexgetSaveFileName = qr/
+           ^(\s*)                        # (1) Indentation
+           (.*?)                         # (2) Possibly "Classname *" (the ? means non-greedy)
+           (\w+)                         # (3) variable name
+           \s*=\s*                       #   assignment
+           KFileDialog::getSaveFileName\s*\((.*)\)  # (4)  KFileDialog::getSaveFileName(...)
+           (.*)$                         # (5) afterreg
+           /x; # /x Enables extended whitespace mode
+        if (my ($indent, $left, $var, $argument, $afterreg) = $_ =~ $regexgetSaveFileName) {
+           warn "QFileDialog::getSaveFileName found\n";
+           my $constructor_regexp = qr/
+                                 ^([^,]*)           # (1) startUrl
+                                 (?:,\s*([^,]*))?   # (2) filter
+                                 (?:,\s*([^,]*))?   # (3) parent
+                                 (?:,\s*([^,]*))?   # (4) caption
+                                 (?:,\s*([^,]*))?   # (5) option
+                                 (.*)$              # (6) after
+                                 /x;
+           my ($starturl, $filter, $parent, $caption, $option, $after);
+           if ( ($starturl, $filter, $parent, $caption, $option, $after) = $argument =~  $constructor_regexp ) {
+              $_ = $indent . $left . $var . " = QFileDialog::getSaveFileName(";
+
+              #QWidget * parent = 0, const QString & caption = QString(), const QString & dir = QString(), const QString & filter = QString(), QString * selectedFilter = 0, Options options = 0
+              if (defined $parent) {
+                 $_ .= "$parent";
+              } else {
+                 $_ .= "0";
+              }
+              if (defined $caption) {
+                 $_ .= ", $caption";
+              } else {
+                 $_ .= ", QString()";
+              }
+              if (defined $starturl) {
+                 $_ .= ", $starturl";
+              } else {
+                 $_ .= ", QString()";
+              }
+              if (defined $filter) {
+                 $_ .= ", $filter";
+              } else {
+                 $_ .= ", QString()";
+              }
+              if (defined $option) {
+                 $option =~ s, ,,;
+                 if ($option =~ /KFileDialog::ConfirmOverwrite/) {
+                    $_ .=");\n"
+                 } else {
+                   warn "$file : QFileDialog::getSaveFileName : fix me option \'$option\'\n";
+                   $_ .= ", 0, $option);\n"; # TODO fix option
+                 }
+              } else {
+                 warn "$file : QFileDialog::getSaveFileName : fix me option \'$option\'\n";
+                 $_ .= ", 0, $option);\n"; # TODO fix option
+              }
+
+
+
+           }
+        }
+
+
 
         $modified ||= $orig ne $_;
         $_;
