70464967a94437c70b302e98c5de5a9be7babc8d
Dan Leinir Turthra Jensen <admin@leinir.dk>
2015-02-21 11:12:32 +0000

    Add leinir

--- relicensecheck.pl
+++ relicensecheck.pl
@@ -82,6 +82,7 @@ my %license_table = (
     'kossebau'  => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'kylafas'   => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'lueck'     => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
+    'leinir'    => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'mbritton'  => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
  'michaelhowell'=> ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
  'michalhumpula'=> ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
