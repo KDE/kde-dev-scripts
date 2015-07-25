8b7176412f557674510e998b25dee8c8cf7f1b23
David Faure <faure@kde.org>
2015-07-25 15:29:06 +0200

    Antonio said yes to all 5 questions - in person ;-)
    CCMAIL: larrosa@kde.org

--- relicensecheck.pl
+++ relicensecheck.pl
@@ -35,6 +35,7 @@ my %license_table = (
     'alexmerry'     => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'alund'         => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'amth'          => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
+    'antlarr'       => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'apol'          => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'aseigo'        => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
     'asserhal'      => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV' ],
