715d65be4f48ba604a8b4fa817dfa292378164d1
Montel Laurent <montel@kde.org>
2017-03-28 14:46:01 +0200

    convert to modern method

--- kf5/convert-kdebug-with-argument.sh
+++ kf5/convert-kdebug-with-argument.sh
@@ -14,62 +14,6 @@ debugnamefile="$4"
 
 upname=`echo $debugnamefile | tr 'a-z' 'A-Z'`
 
-# create debug.h file
-cat > $debugnamefile.h <<EOF
-/*  This file is part of the KDE project
-    Copyright (C) 2015 Laurent Montel <montel@kde.org>
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
-*/
-
-#ifndef ${upname}_H
-#define ${upname}_H
-
-#include <QLoggingCategory>
-Q_DECLARE_LOGGING_CATEGORY($newcategoryname)
-
-#endif
-EOF
-
-
-#create debug.cpp file
-cat > $debugnamefile.cpp <<EOF
-/*  This file is part of the KDE project
-    Copyright (C) 2015 Laurent Montel <montel@kde.org>
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
-*/
-
-#include "$debugnamefile.h"
-Q_LOGGING_CATEGORY($newcategoryname, "$newlogname")
-EOF
-
 #convert debug to new qCDebug/qCWarning/qCCritical
 
 find -iname "*.cpp" -o -iname "*.h" | xargs sed -ri \
@@ -77,12 +21,10 @@ find -iname "*.cpp" -o -iname "*.h" | xargs sed -ri \
 		-e "s,k(Debug|Warning)\s*\(\s*$oldcategorynumber\s*\),qC\1\($newcategoryname\)," \
 		-e "s,k(Fatal|Error)\s*\(\s*$oldcategorynumber\s*\),qCCritical\($newcategoryname\),"
 
-git add $debugnamefile.cpp $debugnamefile.h
-
 
 # end
-echo "1) Add $debugnamefile.cpp to CMakeLists.txt"
-echo "2) Add #include <QDebug> + #include \"${debugnamefile}.h\" to each file which was changed and where it is missing"
+echo "1) add ecm_qt_declare_logging_category(<file>_SRCS HEADER $debugnamefile.h IDENTIFIER $newcategoryname CATEGORY_NAME $newlogname) to CMakeLists.txt";
+echo "2) #include \"${debugnamefile}.h\" to each file which was changed and where it is missing"
 echo "3) Verify that it compiles :)"
 echo "4) If you find bugs fix them please"
 
