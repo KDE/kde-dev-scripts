6efe8e2cc43821ae33481096071c85afaad5dc2a
Pino Toscano <pino@kde.org>
2018-07-03 06:05:19 +0200

    kdelnk2desktop.py: convert to print as function
    Assume Python >= 2.7, and use print() as function, for Python 3
    compatibility.

--- kdelnk2desktop.py
+++ kdelnk2desktop.py
@@ -3,14 +3,14 @@
 import os, sys, string
 
 def help():
-	print "Usage: %s <filename>.kdelnk ..." % sys.argv[0]
+	print("Usage: %s <filename>.kdelnk ..." % sys.argv[0])
 
 if len(sys.argv) < 2:
 	help()
 	sys.exit()
 
 for fn in sys.argv[1:]:
-	print "Doing %s ..." % fn
+	print("Doing %s ..." % fn)
 	f = open(fn, 'r').readlines()
 
 	if string.find(f[0], "# KDE Config") == 0:
