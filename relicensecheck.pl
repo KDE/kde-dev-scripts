1f8a8492eae39d978fecdfba5ad6ad6f0dcc83c6
Thomas Schoeps <tom.schoeps@gmail.com>
2019-02-23 16:54:57 +0100

    Add myself (thomassc) to relicensecheck.pl

--- relicensecheck.pl
+++ relicensecheck.pl
@@ -48,7 +48,8 @@ my %license_table = (
     'rkflx'         => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV', 'CCBYSA4' ],
     'safaalfulaij'  => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV', 'CCBYSA4' ],
     'sitter'        => ['CCBYSA4'],
-    'sstjames'      => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV', 'CCBYSA4' ]
+    'sstjames'      => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV', 'CCBYSA4' ],
+    'thomassc'      => ['gplv23', 'lgplv23', 'gplv2+', 'lgplv2+', '+eV', 'CCBYSA4' ]
 );
 
 my %old_license_table_2 = (
