72ac51d473952939e3dad6941a6f2926eb2c198c
Laurent Montel <montel@kde.org>
2019-06-18 08:45:51 +0200

    Remove KItinerary prefix

--- kf5/clean-includes.sh
+++ kf5/clean-includes.sh
@@ -105,6 +105,7 @@ do
        new=`echo "$new" |perl -pi -e 's!KCodecs/!!g'`;
        new=`echo "$new" |perl -pi -e 's!KSMTP/!!g'`;
        new=`echo "$new" |perl -pi -e 's!KGAPI/!!g'`;
+       new=`echo "$new" |perl -pi -e 's!KItinerary/!!g'`;
        newname=`echo "$new" |perl -pi -e 's!.h!!'`;
 
        #echo "before go : $new";
