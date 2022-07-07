cbb8f67e41b873198864667e0166ce2b1bbc98b8
Volker Krause <vkrause@kde.org>
2022-07-07 16:41:50 +0200

    Add script for applying Qt6/KF6 QML changes

---
+++ kf6/convert-kirigami.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2022 Volker Krause <vkrause@kde.org>
+# SPDX-License-Identifier: CC0-1.0
+
+# icon property changes
+perl -0777 -p -i -e 's/(BasicListItem \{[^\}]*?)(icon): /\1icon.name: /sg' $1
+
+# QtGraphicalEffects
+# rename import
+perl -p -i -e 's/import QtGraphicalEffects 1\.\d+/import Qt5Compat.GraphicalEffects 6.0/' $1
+# remove obsolete samples: property
+perl -0777 -p -i -e 's/(DropShadow \{[^\}].*?)\n\s*samples: \d+/\1/sg' $1
