6b105946db86e6dbfa1de5b13332a819203fcd19
Laurent Montel <montel@kde.org>
2023-06-16 07:21:16 +0200

    Improve porting script

--- kf6/port_plasma_kirigami.sh
+++ kf6/port_plasma_kirigami.sh
@@ -4,10 +4,32 @@ git grep -l "PlasmaCore.Theme.smallestFont" | xargs perl -pi -e 's,PlasmaCore.Th
 git grep -l "PlasmaCore.Theme.NormalColorGroup" | xargs perl -pi -e 's,PlasmaCore.Theme.NormalColorGroup,Kirigami.Theme.Window,g'
 git grep -l "PlasmaCore.ColorScope.colorGroup" | xargs perl -pi -e 's,PlasmaCore.ColorScope.colorGroup,Kirigami.Theme.colorSet,g'
 git grep -l "PlasmaCore.ColorScope.inherit" | xargs perl -pi -e 's,PlasmaCore.ColorScope.inherit,Kirigami.Theme.inherit,g'
-git grep -l "PlasmaCore.Units." | xargs perl -pi -e 's,PlasmaCore.Units.,Kirigami.Units.,'
+git grep -l "PlasmaCore.Units.largeSpacing" | xargs perl -pi -e 's,PlasmaCore.Units.largeSpacing,Kirigami.Units.gridUnit,g'
+git grep -l "PlasmaCore.Units." | xargs perl -pi -e 's,PlasmaCore.Units.,Kirigami.Units.,g'
+git grep -l "PlasmaCore.ColorScope.inherit" | xargs perl -pi -e 's,PlasmaCore.ColorScope.inherit,Kirigami.Theme.inherit,g'
+git grep -l "PlasmaCore.Theme." | xargs perl -pi -e 's,PlasmaCore.Theme.,Kirigami.Theme.,g'
+
+git grep -l "PlasmaCore.Units.devicePixelRatio" | xargs perl -pi -e 's,PlasmaCore.Units.devicePixelRatio,1,g'
+git grep -l "height: PlasmaCore.Units.roundToIconSize" | xargs perl -pi -e 's,height: PlasmaCore.Units.roundToIconSize\(.*\),height: Kirigami.Units.iconSizes.sizeForLabels,g'
+git grep -l "PlasmaCore.Units.roundToIconSize" | xargs perl -pi -e 's,PlasmaCore.Units.roundT   oIconSize,Kirigami.Units.roundedIconSize,g'
+
 
 # Add import org.kde.kirigami 2.20 as Kirigami
-git grep -l "as PlasmaCore" | xargs perl -pi -e 's,as PlasmaCore,as PlasmaCore\nimport org.kde.kirigami 2.20 as Kirigami,'
+
+list_import=`git grep -l "as PlasmaCore"`;
+echo " list file which has import PlasmaCore $list_import";
+for i in $list_import; do
+        echo "list2222222:::: $i";
+        number=`grep 'mport org.kde.kirigami 2.20 as Kirigami' $i|wc -l`;
+        echo $number;
+        if test $number == 0 ; then
+               echo "insert import org.kde.kirigami 2.20 as Kirigam in $i";
+              perl -pi -e 's,as PlasmaCore,as PlasmaCore\nimport org.kde.kirigami 2.20 as Kirigami,' $i;
+        fi
+done;
+
+
+#git grep -l "as PlasmaCore" | xargs perl -pi -e 's,as PlasmaCore,as PlasmaCore\nimport org.kde.kirigami 2.20 as Kirigami,'
 list_PlasmaKirigami_file=`git grep -l kirigami`;
 echo " list file which has kirigami: $list_PlasmaKirigami_file";
 for i in $list_PlasmaKirigami_file; do
