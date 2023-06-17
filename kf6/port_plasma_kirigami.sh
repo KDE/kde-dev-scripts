2f5d516e06061954d03cd3ebd7d5f31b5ff7aecb
Laurent Montel <montel@kde.org>
2023-06-17 07:34:42 +0200

    Fix scripting
    (cherry picked from commit 81a1532f42aaa986ea2b16b5ee8207fdafba8e61)

--- kf6/port_plasma_kirigami.sh
+++ kf6/port_plasma_kirigami.sh
@@ -5,13 +5,13 @@ git grep -l "PlasmaCore.Theme.NormalColorGroup" | xargs perl -pi -e 's,PlasmaCor
 git grep -l "PlasmaCore.ColorScope.colorGroup" | xargs perl -pi -e 's,PlasmaCore.ColorScope.colorGroup,Kirigami.Theme.colorSet,g'
 git grep -l "PlasmaCore.ColorScope.inherit" | xargs perl -pi -e 's,PlasmaCore.ColorScope.inherit,Kirigami.Theme.inherit,g'
 git grep -l "PlasmaCore.Units.largeSpacing" | xargs perl -pi -e 's,PlasmaCore.Units.largeSpacing,Kirigami.Units.gridUnit,g'
+git grep -l "PlasmaCore.Units.roundToIconSize" | xargs perl -pi -e 's,PlasmaCore.Units.roundToIconSize,Kirigami.Units.roundedIconSize,g'
+git grep -l "height: PlasmaCore.Units.roundToIconSize" | xargs perl -pi -e 's,height: PlasmaCore.Units.roundToIconSize\(.*\),height: Kirigami.Units.iconSizes.sizeForLabels,g'
+git grep -l "PlasmaCore.Units.devicePixelRatio" | xargs perl -pi -e 's,PlasmaCore.Units.devicePixelRatio,1,g'
 git grep -l "PlasmaCore.Units." | xargs perl -pi -e 's,PlasmaCore.Units.,Kirigami.Units.,g'
 git grep -l "PlasmaCore.ColorScope.inherit" | xargs perl -pi -e 's,PlasmaCore.ColorScope.inherit,Kirigami.Theme.inherit,g'
 git grep -l "PlasmaCore.Theme." | xargs perl -pi -e 's,PlasmaCore.Theme.,Kirigami.Theme.,g'
 
-git grep -l "PlasmaCore.Units.devicePixelRatio" | xargs perl -pi -e 's,PlasmaCore.Units.devicePixelRatio,1,g'
-git grep -l "height: PlasmaCore.Units.roundToIconSize" | xargs perl -pi -e 's,height: PlasmaCore.Units.roundToIconSize\(.*\),height: Kirigami.Units.iconSizes.sizeForLabels,g'
-git grep -l "PlasmaCore.Units.roundToIconSize" | xargs perl -pi -e 's,PlasmaCore.Units.roundT   oIconSize,Kirigami.Units.roundedIconSize,g'
 
 
 # Add import org.kde.kirigami 2.20 as Kirigami
