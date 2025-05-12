8c29421b6986649c0a53b7510cf74f73270fbeb3
Yelsin Sepulveda <yelsinsepulveda@gmail.com>
2025-05-12 06:15:56 -0400

    Feature: Add support for X11 in matrix_colored_paste
    Detect the current display server, `X11/Wayland`
    
    Use `wl-clipboard` or `xclip` to perform clipboard text transformation based on display server

--- matrix_colored_paste.sh
+++ matrix_colored_paste.sh
@@ -5,8 +5,8 @@
 #   For example, users could copy the terminal output of a problem happening when building projects with kde-builder, and then send it to developers on Matrix.
 
 # Setup:
-#   It is assumed you use Wayland session.
-#   Install wl-clipboard with your package manager.
+#   It is assumed you use Wayland or X11 session.
+#   With your package manager install wl-clipboard for Wayland, or xclip for X11.
 #   In system settings, in keyboard shortcuts, add a new custom shortcut pointing to this script. Let's say it is Meta+U.
 
 # Usage:
@@ -21,9 +21,25 @@
 #   We remove style attributes, while converting colors to matrix attributes, and we remove "uncolored" background and font
 #   (otherwise appeared as forced to black text on white background), so it looks nice in chat with both dark and light themes.
 
-echo "/html $(wl-paste --type text/html)" | \
+# Detect session type
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    html=$(wl-paste --type text/html)
+elif [ "$XDG_SESSION_TYPE" = "x11" ]; then
+    html=$(xclip -selection clipboard -t text/html -o)
+else
+    echo "Unsupported session type: $XDG_SESSION_TYPE"
+    exit 1
+fi
+
+echo "/html $html" | \
 sed 's/font-weight:bold;//g' | \
 sed -E 's/style="color:#([0-9A-Fa-f]{6});background-color:#([0-9A-Fa-f]{6});"/data-mx-color="#\1" data-mx-bg-color="#\2"/g' | \
 sed 's/ data-mx-bg-color="#ffffff"//g' | \
 sed 's/ data-mx-color="#000000"//g' | \
-wl-copy
+{ 
+    if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+        wl-copy
+    else
+        xclip -selection clipboard
+    fi
+}
